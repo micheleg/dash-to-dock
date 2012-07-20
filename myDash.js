bd3a8ea6157522c2e4a282a1bbba5e9c7435570d
Michele <micxgx@gmail.com>
2012-07-20 15:09:51 +0200

    Extend appWellIcon emitting "menu-closed" signal.
    The signal is now added to the extended appWellIcon.

--- myDash.js
+++ myDash.js
@@ -202,7 +202,7 @@ const myDash = new Lang.Class({
     },
 
     _createAppItem: function(app) {
-        let display = new myAppWellIcon(app,
+        let display = new myAppWellIcon(this, app,
                                                  { setSizeManually: true,
                                                    showLabel: false });
         display._draggable.connect('drag-begin',
@@ -692,8 +692,23 @@ Signals.addSignalMethods(myDash.prototype);
 /**
  * Extend AppWellIcon
  *
+ * - emit "menu-closed" signal on popup menu close.
  */
 const myAppWellIcon = new Lang.Class({
     Name: 'dashToDock.AppWellIcon',
-    Extends: AppDisplay.AppWellIcon
+    Extends: AppDisplay.AppWellIcon,
+
+    // a good parent object is needed to emit the 'menu-closed' signal
+    _init: function(parentObject, app, iconParams, onActivateOverride) {
+
+        this.parent(app, iconParams, onActivateOverride);
+
+        // Emit a custom signal when a menu is closed
+        let _onMenuOpenStateOriginal = this._menuManager._onMenuOpenState;
+        this._menuManager._onMenuOpenState = function(menu, open){
+            if(!open)
+                parentObject.emit('menu-closed');
+            Lang.bind(this, _onMenuOpenStateOriginal)(menu, open);
+        }
+    }
 });
