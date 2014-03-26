2565828b47019e5b96c89b8462a0dbd5bb7bc60c
passingthru67 <passingthru67@gmail.com>
2014-03-26 10:08:16 -0400

    Emit signals for when the dock is triggered to show or hide.
    These signals are not used by the extension itself, they are provided for
    compatibility with other extensions - notably
    workspaces-to-dock@passingthru67.gmail.com - which can thus connect to this
    signals to know when the dash is shown or hide.

--- dockedDash.js
+++ dockedDash.js
@@ -479,6 +479,7 @@ const dockedDash = new Lang.Class({
                 delay = 0;
             }
 
+            this.emit("showing");
             this._animateIn(this._settings.get_double('animation-time'), delay);
         }
     },
@@ -510,6 +511,7 @@ const dockedDash = new Lang.Class({
                 delay = this._settings.get_double('hide-delay');
             }
 
+            this.emit("hiding");
             this._animateOut(this._settings.get_double('animation-time'), delay);
 
         }
