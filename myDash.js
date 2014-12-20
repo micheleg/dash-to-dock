50c0d82829c39668819dc08e9292b23fc4cb8625
Michele <micxgx@gmail.com>
2014-12-20 14:14:15 +0000

    myDash: extend the DashItemContainer.
    To manage laebel according to the dock position.

--- myDash.js
+++ myDash.js
@@ -66,6 +66,84 @@ const myAppIconMenu = new Lang.Class({
     }
 });
 
+/**
+ * Extend DashItemContainer
+ *
+ * - Pass settings to the constructor
+ * - set label position based on dash orientation
+ *
+ */
+const myDashItemContainer = new Lang.Class({
+    Name: 'dashToDockDashItemContainer',
+    Extends: Dash.DashItemContainer,
+
+    _init: function(settings) {
+      this._settings = settings;
+      this.parent();
+    },
+
+    showLabel: function() {
+      if (!this._labelText) {
+        return;
+      }
+
+      this.label.set_text(this._labelText);
+      this.label.opacity = 0;
+      this.label.show();
+
+      let [stageX, stageY] = this.get_transformed_position();
+      let node = this.label.get_theme_node();
+
+      let itemWidth  = this.allocation.x2 - this.allocation.x1;
+      let itemHeight = this.allocation.y2 - this.allocation.y1;
+
+      
+      let labelWidth = this.label.get_width();
+      let labelHeight = this.label.get_height();
+
+      let x, y, xOffset, yOffset;
+
+      let position = getPosition(this._settings);
+        this._isHorizontal = ( position == St.Side.TOP ||
+                               position == St.Side.BOTTOM);
+      let labelOffset = node.get_length('-x-offset');
+
+      switch(position) {
+        case St.Side.LEFT:
+            yOffset = Math.floor((itemHeight - labelHeight) / 2)
+            y = stageY + yOffset;
+            xOffset = labelOffset;
+            x = stageX + this.get_width() + xOffset;
+            break;
+          break;  
+        case St.Side.RIGHT:
+            yOffset = Math.floor((itemHeight - labelHeight) / 2)
+            y = stageY + yOffset;
+            xOffset = labelOffset;
+            x = Math.round(stageX) - labelWidth - xOffset;
+            break;
+        case St.Side.TOP:
+            y = stageY + labelOffset + itemHeight;
+            xOffset = Math.floor((itemWidth - labelWidth) / 2);
+            x = stageX + xOffset;
+            break;
+        case St.Side.BOTTOM:
+            yOffset = labelOffset;
+            y = stageY - labelHeight - yOffset;
+            xOffset = Math.floor((itemWidth - labelWidth) / 2);
+            x = stageX + xOffset;
+            break;
+      }
+
+      this.label.set_position(x, y);
+      Tweener.addTween(this.label,
+        { opacity: 255,
+          time: DASH_ITEM_LABEL_SHOW_TIME,
+          transition: 'easeOutQuad',
+        });
+      }
+});
+
 /* This class is a fork of the upstream DashActor class (ui.dash.js)
  *
  * Summary of changes:
@@ -363,7 +441,7 @@ const myDash = new Lang.Class({
                             this._itemMenuStateChanged(item, opened);
                         }));
 
-        let item = new Dash.DashItemContainer();
+        let item = new myDashItemContainer(this._settings);
         item.setChild(appIcon.actor);
 
         // Override default AppIcon label_actor, now the
