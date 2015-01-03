47f03c9bd0da2191aae84a800ba080b7551580ab
Michele <micxgx@gmail.com>
2015-01-03 15:57:16 +0000

    Scroll workspace works correclty also in bottom and right dock.

--- dockedDash.js
+++ dockedDash.js
@@ -1046,12 +1046,20 @@ const dockedDash = new Lang.Class({
 
                 let [x,y] = event.get_coords();
 
-                if (this._rtl) {
-                    if(x < this.staticBox.x2 - 1)
-                        return false;
-                } else {
-                    if(x > this.staticBox.x1 + 1)
-                        return false;
+                if ( ( this._position == St.Side.RIGHT &&
+                       x < this.staticBox.x2 - 1
+                     ) ||
+                     ( this._position == St.Side.LEFT &&
+                       x > this.staticBox.x1 + 1
+                     ) ||
+                     ( this._position == St.Side.BOTTOM &&
+                       y < this.staticBox.y2 - 1
+                     ) ||
+                     ( this._position == St.Side.TOP &&
+                       y > this.staticBox.y1 + 1
+                     )
+                   ) {
+                    return false;
                 }
             }
 
