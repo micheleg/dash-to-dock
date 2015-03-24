636196061fbadb08f7adce56018ccf1e80bd5e85
Michele <micxgx@gmail.com>
2015-03-24 01:14:06 +0000

    Remove showing animation delay already handled by the pressure/dwell system.
    I don't need a show delay any more, or better, it's now the timeout of
    either the barrier or the dwelling system which achieve the same effect.

--- dockedDash.js
+++ dockedDash.js
@@ -29,9 +29,7 @@ const Convenience = Me.imports.convenience;
 const Intellihide = Me.imports.intellihide;
 const MyDash = Me.imports.myDash;
 
-const PRESSURE_TIMEOUT = 1000;
 const DOCK_DWELL_CHECK_INTERVAL = 100;  //TODO
-const DOCK_DWELL_TIME = 250; //TODO
 
 /* Return the actual position reverseing left and right in rtl */
 function getPosition(settings) {
@@ -708,19 +706,13 @@ const dockedDash = new Lang.Class({
 
         if(  anim.hidden() || anim.hiding()  ){
 
-            let delay;
-            // If the dock is hidden, wait this._settings.get_double('show-delay') before showing it; 
-            // otherwise show it immediately.
-            if(anim.hidden()){
-                delay = this._settings.get_double('show-delay');
-            } else if(anim.hiding()){
+            if(anim.hiding()){
                 // suppress all potential queued hiding animations (always give priority to show)
                 this._removeAnimations();
-                delay = 0;
             }
 
             this.emit("showing");
-            this._animateIn(this._settings.get_double('animation-time'), delay);
+            this._animateIn(this._settings.get_double('animation-time'), 0);
         }
     },
 
@@ -737,16 +729,10 @@ const dockedDash = new Lang.Class({
             // over the screen  border but then went away, i.e not a sufficient 
             // amount of time is passeed to trigger the dock showing) remove it.
             if( anim.showing()) {
-                if(anim.running){
-                    //if a show already started, let it finish; queue hide without removing the show.
-                    // to obtain this I increase the delay to avoid the overlap and interference 
-                    // between the animations
-                    delay = this._settings.get_double('hide-delay') + 1.2*this._settings.get_double('animation-time') + this._settings.get_double('show-delay');
-
-                } else {
-                    this._removeAnimations();
-                    delay = 0;
-                }
+                //if a show already started, let it finish; queue hide without removing the show.
+                // to obtain this I increase the delay to avoid the overlap and interference 
+                // between the animations
+                delay = this._settings.get_double('hide-delay') + 1.2*this._settings.get_double('animation-time');
             } else if( anim.shown() ) {
                 delay = this._settings.get_double('hide-delay');
             }
@@ -843,7 +829,7 @@ const dockedDash = new Lang.Class({
                 let focusWindow = global.display.focus_window;
                 this._dockDwellUserTime = focusWindow ? focusWindow.user_time : 0;
 
-                this._dockDwellTimeoutId = Mainloop.timeout_add(DOCK_DWELL_TIME,
+                this._dockDwellTimeoutId = Mainloop.timeout_add(this._settings.get_double('show-delay')*1000,
                                                                 Lang.bind(this, this._dockDwellTimeout));
                 GLib.Source.set_name_by_id(this._dockDwellTimeoutId, '[dash-to-dock] this._dockDwellTimeout');
             }
@@ -897,7 +883,7 @@ const dockedDash = new Lang.Class({
 
         // Create new pressure barrier based on pressure threshold setting
         if (this._canUsePressure) {
-            this._pressureBarrier = new Layout.PressureBarrier(pressureThreshold, PRESSURE_TIMEOUT,
+            this._pressureBarrier = new Layout.PressureBarrier(pressureThreshold, this._settings.get_double('show-delay')*1000,
                                 Shell.KeyBindingMode.NORMAL | Shell.KeyBindingMode.OVERVIEW);
             this._pressureBarrier.connect('trigger', Lang.bind(this, function(barrier){
                 this._onPressureSensed();
