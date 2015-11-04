28cb2778a84bfd3387bd342715b4ff87909371e4
Michele <micxgx@gmail.com>
2015-11-04 00:26:45 +0000

    appIcons: improve transition from populated to empty windows previews.
    Set the menu as insensitive instead of hiding it when the last windows
    of the application is destroyed. This result in a smoother transition,
    without the menu being resized for example.

--- appIcons.js
+++ appIcons.js
@@ -572,6 +572,7 @@ const MyAppIconMenu = new Lang.Class({
             // of the current desktop and other windows.
 
             this._allWindowsMenuItem = new PopupMenu.PopupSubMenuMenuItem(_('All Windows'), false);
+            this._allWindowsMenuItem.actor.hide();
             this.addMenuItem(this._allWindowsMenuItem);
 
             if (!this._source.app.is_window_backed()) {
@@ -703,13 +704,13 @@ const MyAppIconMenu = new Lang.Class({
               // Temporary workaround: add 15px to compensate
               this._allWindowsMenuItem.actor.width =  this._allWindowsMenuItem.menu.actor.width + 15;
 
-              }
+          }
 
+          // The menu is created hidden and never hidded after being shown. Instead, a singlal
+          // connected to its items destroy will set is insensitive if no more windows preview are shown.
           if (windows.length > 0){
               this._allWindowsMenuItem.actor.show();
-          } else {
-              this._allWindowsMenuItem.actor.hide();
-              this._allWindowsMenuItem.menu.close();
+              this._allWindowsMenuItem.setSensitive(true);
           }
 
           // Update separators
@@ -739,6 +740,12 @@ const MyAppIconMenu = new Lang.Class({
                     this._allWindowsMenuItem.menu.addMenuItem(item);
                     item.connect('activate', Lang.bind(this, function() {
                         this.emit('activate-window', window);
+                    }));
+
+                    // This is to achieve a more gracefull transition when the last windows is closed.
+                    item.connect('destroy', Lang.bind(this, function() {
+                        if(this._allWindowsMenuItem.menu._getMenuItems().length == 1) // It's still counting the item just going to be destroyed
+                            this._allWindowsMenuItem.setSensitive(false);
                     }));
                 }
             }
