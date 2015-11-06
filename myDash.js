e97197dbdd924e479a9eadc47aded413655c045c
Michele <micxgx@gmail.com>
2015-11-06 22:02:04 +0000

    myDash: use option for showing windows previews.

--- myDash.js
+++ myDash.js
@@ -54,13 +54,13 @@ function getPosition(settings) {
  * - update menu when application windows change
  */
 
-const SHOW_WINDOWS_PREVIEW = true;
 const myAppIconMenu = new Lang.Class({
     Name: 'myAppIconMenu',
     Extends: AppDisplay.AppIconMenu,
 
     _init: function(source, settings) {
 
+        this._settings = settings;
         let side = getPosition(settings);
 
         // Damm it, there has to be a proper way of doing this...
@@ -82,7 +82,7 @@ const myAppIconMenu = new Lang.Class({
     _redisplay: function() {
         this.removeAll();
 
-        if (SHOW_WINDOWS_PREVIEW) {
+        if (this._settings.get_boolean('show-windows-preview')) {
             // Display the app windows menu items and the separator between windows
             // of the current desktop and other windows.
 
@@ -182,7 +182,7 @@ const myAppIconMenu = new Lang.Class({
     // acting on windows (closing) are performed while the menu is shown.
     update: function() {
 
-      if(SHOW_WINDOWS_PREVIEW){
+      if(this._settings.get_boolean('show-windows-preview')){
 
           let windows = this._source.app.get_windows().filter(function(w) {
               return !w.skip_taskbar;
