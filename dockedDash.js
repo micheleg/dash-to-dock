micheleg/dash-to-dock

commit 976bafaee28180a0102238589087d72817a78233

Track structs of the outer dash.actor instead of the inner dash._box.
Previously the padding of the container was also taken into account.

dockedDash.js
@@ -343,7 +343,7 @@ const dockedDash = new Lang.Class({
         this.actor.raise(global.top_window_group);
 
         if ( this._settings.get_boolean('dock-fixed') )
-          Main.layoutManager._trackActor(this.dash._box, {affectsStruts: true});
+          Main.layoutManager._trackActor(this.dash.actor, {affectsStruts: true});
 
         // pretend this._slider is isToplevel child so that fullscreen is actually tracked
         let index = Main.layoutManager._findActor(this._slider);
@@ -431,11 +431,11 @@ const dockedDash = new Lang.Class({
         this._settings.connect('changed::dock-fixed', Lang.bind(this, function(){
 
             if(this._settings.get_boolean('dock-fixed')) {
-                Main.layoutManager._trackActor(this.dash._box, {affectsStruts: true});
+                Main.layoutManager._trackActor(this.dash.actor, {affectsStruts: true});
                 // show dash
                 this.disableAutoHide();
             } else {
-                Main.layoutManager._untrackActor(this.dash._box);
+                Main.layoutManager._untrackActor(this.dash.actor);
                 this.emit('box-changed');
             }
 

commit 3e154f1a64e5b4cf36f5f1db6379c59ed51aab4c

Track the struts of the dashtodokBox.
Commit 976bafaee28180a0102238589087d72817a78233 introduced a bug a long
time ago. The dash.actor is larger than the visible dash, as it is used
with a bindConstrain to set the dash max size. The result is that in
fixed mode the area below the dash become insensitive.

I moved to track this actor to avoid including the 1px padding once
required to show the dock. This is not a problem anymore (I'm using the
pressure barrier/dwelling sytem now), so reverting to tracking the
dashtodockBox fix this problem.

dockedDash.js
@@ -459,7 +459,7 @@ const dockedDash = new Lang.Class({
         Main.layoutManager.uiGroup.set_child_below_sibling(this.actor,Main.layoutManager.modalDialogGroup);
 
         if ( this._settings.get_boolean('dock-fixed') )
-          Main.layoutManager._trackActor(this.dash.actor, {affectsStruts: true, trackFullscreen: true});
+          Main.layoutManager._trackActor(this._box, {affectsStruts: true, trackFullscreen: true});
 
         // pretend this._slider is isToplevel child so that fullscreen is actually tracked
         let index = Main.layoutManager._findActor(this._slider.actor);
@@ -588,9 +588,9 @@ const dockedDash = new Lang.Class({
         this._settings.connect('changed::dock-fixed', Lang.bind(this, function(){
 
             if(this._settings.get_boolean('dock-fixed')) {
-                Main.layoutManager._trackActor(this.dash.actor, {affectsStruts: true, trackFullscreen: true});
+                Main.layoutManager._trackActor(this._box, {affectsStruts: true, trackFullscreen: true});
             } else {
-                Main.layoutManager._untrackActor(this.dash.actor);
+                Main.layoutManager._untrackActor(this._box);
             }
 
             this._resetPosition();