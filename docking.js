micheleg/dash-to-dock

commit 0f2fa2fd05a2bd5d9047cccbc25fc0bd253d4ff8

Further limit sensitive area avoiding hotcornes.
Following 393c4acc09f405abdf2378687fb5d59f0385a35a, skip 1 px on each
screen edge to avoid conflicting with hotcorners (e.g. Activities hotcorner
with the panel hidden by some extension or other custom hotcorners, or
the legacyTry).

docking.js
@@ -778,16 +778,16 @@ const DockedDash = new Lang.Class({
 
         let workArea = Main.layoutManager.getWorkAreaForMonitor(this._monitor.index)
         let shouldDwell;
-        // Check for the correct screen edge
-        // Position is approximated to the lower integer
+        // Check for the correct screen edge, extending the sensitive area to the whole workarea,
+        // minus 1 px to avoid conflicting with other active corners.
         if (this._position == St.Side.LEFT)
-            shouldDwell = (x == this._monitor.x) && (y >= workArea.y) && (y <= workArea.y + workArea.height);
+            shouldDwell = (x == this._monitor.x) && (y > workArea.y) && (y < workArea.y + workArea.height);
         else if (this._position == St.Side.RIGHT)
-            shouldDwell = (x == this._monitor.x + this._monitor.width - 1) && (y >= workArea.y) && (y <= workArea.y + workArea.height);
+            shouldDwell = (x == this._monitor.x + this._monitor.width - 1) && (y > workArea.y) && (y < workArea.y + workArea.height);
         else if (this._position == St.Side.TOP)
-            shouldDwell = (y == this._monitor.y) && (x >= workArea.x) && (x <= workArea.x + workArea.width);
+            shouldDwell = (y == this._monitor.y) && (x > workArea.x) && (x < workArea.x + workArea.width);
         else if (this._position == St.Side.BOTTOM)
-            shouldDwell = (y == this._monitor.y + this._monitor.height - 1) && (x >= workArea.x) && (x <= workArea.x + workArea.width);
+            shouldDwell = (y == this._monitor.y + this._monitor.height - 1) && (x > workArea.x) && (x < workArea.x + workArea.width);
 
         if (shouldDwell) {
             // We only set up dwell timeout when the user is not hovering over the dock
@@ -959,37 +959,38 @@ const DockedDash = new Lang.Class({
         }
 
         // Create new barrier
-        // Note: dash in fixed position doesn't use pressure barrier
+        // The barrier extends to the whole workarea, minus 1 px to avoid conflicting with other active corners
+        // Note: dash in fixed position doesn't use pressure barrier.
         if (this._canUsePressure && this._autohideIsEnabled && this._settings.get_boolean('require-pressure-to-show')) {
             let x1, x2, y1, y2, direction;
             let workArea = Main.layoutManager.getWorkAreaForMonitor(this._monitor.index)
 
             if (this._position == St.Side.LEFT) {
-                x1 = this._monitor.x;
-                x2 = this._monitor.x;
-                y1 = workArea.y;
-                y2 = workArea.y + workArea.height;
+                x1 = this._monitor.x + 1;
+                x2 = x1;
+                y1 = workArea.y - 1;
+                y2 = workArea.y + workArea.height - 1;
                 direction = Meta.BarrierDirection.POSITIVE_X;
             }
             else if (this._position == St.Side.RIGHT) {
-                x1 = this._monitor.x + this._monitor.width;
-                x2 = this._monitor.x + this._monitor.width;
-                y1 = workArea.y;
-                y2 = workArea.y + workArea.height;
+                x1 = this._monitor.x + this._monitor.width - 1;
+                x2 = x2;
+                y1 = workArea.y + 1;
+                y2 = workArea.y + workArea.height - 1;
                 direction = Meta.BarrierDirection.NEGATIVE_X;
             }
             else if (this._position == St.Side.TOP) {
-                x1 = workArea.x;
-                x2 = workArea.x + workArea.width;
+                x1 = workArea.x + 1;
+                x2 = workArea.x + workArea.width - 1;
                 y1 = this._monitor.y;
-                y2 = this._monitor.y;
+                y2 = y1;
                 direction = Meta.BarrierDirection.POSITIVE_Y;
             }
             else if (this._position == St.Side.BOTTOM) {
-                x1 = workArea.x;
-                x2 = workArea.x + workArea.width;
+                x1 = workArea.x + 1;
+                x2 = workArea.x + workArea.width - 1;
                 y1 = this._monitor.y + this._monitor.height;
-                y2 = this._monitor.y + this._monitor.height;
+                y2 = y1;
                 direction = Meta.BarrierDirection.NEGATIVE_Y;
             }
 

commit bb271989effeb66d202a133ff610b46da68c879c

Fix bug due to typo in previous commit.
Fix +/- typo introduced in 0f2fa2fd05a2bd5d9047cccbc25fc0bd253d4ff8.

docking.js
@@ -968,7 +968,7 @@ const DockedDash = new Lang.Class({
             if (this._position == St.Side.LEFT) {
                 x1 = this._monitor.x + 1;
                 x2 = x1;
-                y1 = workArea.y - 1;
+                y1 = workArea.y + 1;
                 y2 = workArea.y + workArea.height - 1;
                 direction = Meta.BarrierDirection.POSITIVE_X;
             }