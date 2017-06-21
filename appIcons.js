bccbc4d54dd9974cc66e5965d7731b1359a09967
franglais125 <franglais125@gmail.com>
2017-06-21 13:56:25 -0400

    appIcons.js: we need the 'window-entered-monitor' signal after all.
    This partially revert previous commit:
    058e97d574982dc62a0af7a0b929f8e5aede766a
    
    The reason is that  favorited icons don't get redrawn by a
    dash._queueRedisplay() call, and the focus/running style is not updated.

--- appIcons.js
+++ appIcons.js
@@ -98,6 +98,10 @@ const MyAppIcon = new Lang.Class({
         this._focusAppChangeId = tracker.connect('notify::focus-app',
                                                  Lang.bind(this,
                                                            this._onFocusAppChanged));
+        this._enteredMonitorId = global.screen.connect('window-entered-monitor',
+                                                       Lang.bind(this,
+                                                                 this.onWindowsChanged));
+
         this._dots = null;
 
         let keys = ['apply-custom-theme',
@@ -142,6 +146,11 @@ const MyAppIcon = new Lang.Class({
         if (this._focusAppChangeId > 0) {
             tracker.disconnect(this._focusAppChangeId);
             this._focusAppChangeId = 0;
+        }
+
+        if (this._enteredMonitorId > 0) {
+            global.screen.disconnect(this._enteredMonitorId);
+            this._enteredMonitorId = 0;
         }
 
         this._signalsHandler.destroy();
