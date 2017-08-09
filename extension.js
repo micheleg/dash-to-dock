2f37107ac30039cf3f7218a376c1eb563e66ef78
Didier Roche <didrocks@ubuntu.com>
2017-08-09 14:41:00 +0200

    Enable loading Dash to Dock alongside this extension
    This extension will be virtually and dynamically disabled if Dash to Dock is
    loaded and reenabled once Dash to Dock is disabled.
    We also avoid loading on Shell start if Dash to Dock is in the extension
    enabled list.
    
    Basically, this allow the user to go into the tweak mode by just installing
    that extension.
    Note that some keybindings handler are not properly destroyed on main object
    unload and can make the extension appears in error while still functional.
    This is an upstream Dash to Dock issue that we should fix there.

--- extension.js
+++ extension.js
@@ -3,21 +3,57 @@
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Docking = Me.imports.docking;
 const Convenience = Me.imports.convenience;
+const Gio = imports.gi.Gio;
 
 // We declare this with var so it can be accessed by other extensions in
 // GNOME Shell 3.26+ (mozjs52+).
 var dockManager;
+
+
+let _enabledextensionsettings;
+let _extensionlistenerId;
 
 function init() {
     Convenience.initTranslations('dashtodock');
 }
 
 function enable() {
-    dockManager = new Docking.DockManager();
+    /*
+     * Listen to enabled extension, if Dash to Dock is on the list or become active,
+     * we disable this dock.
+     */
+    _enabledextensionsettings = new Gio.Settings({ schema_id: 'org.gnome.shell' });
+    _extensionlistenerId = _enabledextensionsettings.connect(
+        'changed::enabled-extensions',
+        conditionallyenabledock);
+    conditionallyenabledock();
 }
 
 function disable() {
     dockManager.destroy();
 
     dockManager=null;
+
+    if (_extensionlistenerId) {
+        _enabledextensionsettings.disconnect(_extensionlistenerId);
+        _extensionlistenerId = 0;
+    }
+}
+
+function conditionallyenabledock() {
+    let to_enable = true;
+    let extensions = _enabledextensionsettings.get_strv('enabled-extensions');
+    for (let i = 0; i < extensions.length; i++) {
+        if (extensions[i] === "dash-to-dock@micxgx.gmail.com") {
+            to_enable = false;
+        }
+    }
+
+    // enable or disable dock depending on dock status and to_enable state
+    if (to_enable && !dockManager) {
+        dockManager = new Docking.DockManager();
+    } else if (!to_enable && dockManager) {
+        dockManager.destroy();
+        dockManager = null;
+    }
 }
