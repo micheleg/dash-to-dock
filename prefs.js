micheleg/dash-to-dock

commit 8ccc7d2d55e1b425be605ff3d29052f5a43994ec

Remove the Adaptive transparency mode.
The panel dynamic transparency was drop upstream:

https://gitlab.gnome.org/GNOME/gnome-shell/commit/9cfb51c106abd1f96012bf39f3d329cf060035cf

so this option does not make sense any more.

Settings.ui
@@ -2164,7 +2164,6 @@
                                     <items>
                                       <item translatable="yes">Default</item>
                                       <item translatable="yes">Fixed</item>
-                                      <item translatable="yes">Adaptive</item>
                                       <item translatable="yes">Dynamic</item>
                                     </items>
                                   </object>

prefs.js
@@ -22,7 +22,6 @@ const DEFAULT_ICONS_SIZES = [ 128, 96, 64, 48, 32, 24, 16 ];
 const TransparencyMode = {
     DEFAULT:  0,
     FIXED:    1,
-    ADAPTIVE: 2,
     DYNAMIC:  3
 };
 
@@ -774,14 +773,12 @@ var Settings = class DashToDock_Settings {
                this._builder.get_object('custom_opacity_scale').set_sensitive(true);
         });
 
-        if (this._settings.get_enum('transparency-mode') !== TransparencyMode.ADAPTIVE &&
-            this._settings.get_enum('transparency-mode') !== TransparencyMode.DYNAMIC) {
+        if (this._settings.get_enum('transparency-mode') !== TransparencyMode.DYNAMIC) {
             this._builder.get_object('dynamic_opacity_button').set_sensitive(false);
         }
 
         this._settings.connect('changed::transparency-mode', () => {
-            if (this._settings.get_enum('transparency-mode') !== TransparencyMode.ADAPTIVE &&
-                this._settings.get_enum('transparency-mode') !== TransparencyMode.DYNAMIC) {
+            if (this._settings.get_enum('transparency-mode') !== TransparencyMode.DYNAMIC) {
                 this._builder.get_object('dynamic_opacity_button').set_sensitive(false);
             }
             else {

schemas/org.gnome.shell.extensions.dash-to-dock.gschema.xml
@@ -32,7 +32,6 @@
   <enum id='org.gnome.shell.extensions.dash-to-dock.transparency-mode'>
     <value value='0' nick='DEFAULT'/>
     <value value='1' nick='FIXED'/>
-    <value value='2' nick='ADAPTIVE'/>
     <value value='3' nick='DYNAMIC'/>
   </enum>
     <enum id='org.gnome.shell.extensions.dash-to-dock.running-indicator-style'>
@@ -79,7 +78,7 @@
     <key name="transparency-mode" enum="org.gnome.shell.extensions.dash-to-dock.transparency-mode">
       <default>'DEFAULT'</default>
       <summary>Transparency mode for the dock</summary>
-      <description>FIXED: constant transparency. ADAPTIVE: lock state with the top panel when not hidden. DYNAMIC: dock takes the opaque style only when windows are close to it.</description>
+      <description>FIXED: constant transparency. DYNAMIC: dock takes the opaque style only when windows are close to it.</description>
     </key>
     <key name="running-indicator-style" enum="org.gnome.shell.extensions.dash-to-dock.running-indicator-style">
       <default>'DEFAULT'</default>
@@ -94,7 +93,7 @@
     <key type="b" name="customize-alphas">
       <default>false</default>
       <summary>Manually set the min and max opacity</summary>
-      <description>For Adaptive and Dynamic modes, the min/max opacity values will be given by 'min-alpha' and 'max-alpha'.</description>
+      <description>For the dynamic mode, the min/max opacity values will be given by 'min-alpha' and 'max-alpha'.</description>
     </key>
     <key type="d" name="min-alpha">
       <default>0.2</default>

theming.js
@@ -28,16 +28,11 @@ const Utils = Me.imports.utils;
 /*
  * DEFAULT:  transparency given by theme
  * FIXED:    constant transparency chosen by user
- * ADAPTIVE: apply 'transparent' style to dock AND panel when
- *           no windows are close to the dock OR panel.
- *           When dock is hidden, the dock 'transparent' style only
- *           apply to itself.
  * DYNAMIC:  apply 'transparent' style when no windows are close to the dock
  * */
 const TransparencyMode = {
     DEFAULT:  0,
     FIXED:    1,
-    ADAPTIVE: 2,
     DYNAMIC:  3
 };
 
@@ -166,7 +161,7 @@ var ThemeManager = class DashToDock_ThemeManager {
         if (this._settings.get_boolean('custom-background-color')) {
             // When applying a custom color, we need to check the alpha value,
             // if not the opacity will always be overridden by the color below.
-            // Note that if using 'adaptive' or 'dynamic' transparency modes,
+            // Note that if using 'dynamic' transparency modes,
             // the opacity will be set by the opaque/transparent styles anyway.
             let newAlpha = Math.round(backgroundColor.alpha/2.55)/100;
             if (this._settings.get_enum('transparency-mode') == TransparencyMode.FIXED)
@@ -398,9 +393,6 @@ var Transparency = class DashToDock_Transparency {
             this._onWindowActorAdded(null, win);
         }, this);
 
-        if (this._settings.get_enum('transparency-mode') === TransparencyMode.ADAPTIVE)
-            this._enableAdaptive();
-
         if (this._actor.get_stage())
             this._updateSolidStyle();
 
@@ -411,8 +403,6 @@ var Transparency = class DashToDock_Transparency {
     }
 
     disable() {
-        this._disableAdaptive();
-
         // ensure I never double-register/inject
         // although it should never happen
         this._signalsHandler.removeWithLabel('transparency');
@@ -452,24 +442,15 @@ var Transparency = class DashToDock_Transparency {
     }
 
     _updateSolidStyle() {
-        let isNear = this._dockIsNear() || this._panelIsNear();
-        if (isNear) {
+        if (this._dockIsNear()) {
             this._actor.set_style(this._opaque_style);
             this._dockActor.remove_style_class_name('transparent');
             this._dockActor.add_style_class_name('opaque');
-            if (this._panel._updateSolidStyle && this._adaptiveEnabled) {
-                if (this._settings.get_boolean('dock-fixed') || this._panelIsNear())
-                    this._panel._addStyleClassName('solid');
-                else
-                    this._panel._removeStyleClassName('solid');
-            }
         }
         else {
             this._actor.set_style(this._transparent_style);
             this._dockActor.remove_style_class_name('opaque');
             this._dockActor.add_style_class_name('transparent');
-            if (this._panel._updateSolidStyle && this._adaptiveEnabled)
-                this._panel._removeStyleClassName('solid');
         }
 
         this.emit('solid-style-updated', isNear);
@@ -530,37 +511,6 @@ var Transparency = class DashToDock_Transparency {
         return isNearEnough;
     }
 
-    _panelIsNear() {
-        if (!this._panel._updateSolidStyle ||
-            this._settings.get_enum('transparency-mode') !== TransparencyMode.ADAPTIVE)
-            return false;
-
-        if (this._panel.actor.has_style_pseudo_class('overview') || !Main.sessionMode.hasWindows) {
-            this._panel._removeStyleClassName('solid');
-            return false;
-        }
-
-        /* Get all the windows in the active workspace that are in the
-         * primary monitor and visible */
-        let activeWorkspace = global.workspace_manager.get_active_workspace();
-        let windows = activeWorkspace.list_windows().filter(function(metaWindow) {
-            return metaWindow.is_on_primary_monitor() &&
-                metaWindow.showing_on_its_workspace() &&
-                metaWindow.get_window_type() != Meta.WindowType.DESKTOP;
-        });
-
-        /* Check if at least one window is near enough to the panel */
-        let [, panelTop] = this._panel.actor.get_transformed_position();
-        let panelBottom = panelTop + this._panel.actor.get_height();
-        let scale = St.ThemeContext.get_for_stage(global.stage).scale_factor;
-        let isNearEnough = windows.some((metaWindow) => {
-            let verticalPosition = metaWindow.get_frame_rect().y;
-            return verticalPosition < panelBottom + 5 * scale;
-        });
-
-        return isNearEnough;
-    }
-
     _updateStyles() {
         this._getAlphas();
 
@@ -613,71 +563,6 @@ var Transparency = class DashToDock_Transparency {
             this._transparentAlpha = this._settings.get_double('min-alpha');
             this._transparentAlphaBorder = this._transparentAlpha / 2;
         }
-
-        if (this._settings.get_enum('transparency-mode') === TransparencyMode.ADAPTIVE &&
-            this._panel._updateSolidStyle) {
-            themeNode = this._panel.actor.get_theme_node();
-            if (this._panel.actor.has_style_class_name('solid')) {
-                this._opaqueTransition = themeNode.get_transition_duration();
-                this._panel._removeStyleClassName('solid');
-                themeNode = this._panel.actor.get_theme_node();
-                this._transparentTransition = themeNode.get_transition_duration();
-                this._panel._addStyleClassName('solid');
-            }
-            else {
-                this._transparentTransition = themeNode.get_transition_duration();
-                this._panel._addStyleClassName('solid');
-                themeNode = this._panel.actor.get_theme_node();
-                this._opaqueTransition = themeNode.get_transition_duration();
-                this._panel._removeStyleClassName('solid');
-            }
-        }
-    }
-
-    _enableAdaptive() {
-        if (!this._panel._updateSolidStyle ||
-            this._dash._monitorIndex !== Main.layoutManager.primaryIndex)
-            return;
-
-        this._adaptiveEnabled = true;
-
-        function UpdateSolidStyle() {
-            return;
-        }
-
-        this._injectionsHandler.addWithLabel('adaptive', [
-            this._panel,
-            '_updateSolidStyle',
-            UpdateSolidStyle
-        ]);
-
-        // Once we injected the new function, we need to disconnect and
-        // reconnect all window signals.
-        for (let key of this._panel._trackedWindows.keys())
-            this._panel._trackedWindows.get(key).forEach(id => {
-                key.disconnect(id);
-            });
-
-        for (let win of this._panel._trackedWindows.keys())
-            this._panel._onWindowActorAdded(null, win);
-    }
-
-    _disableAdaptive() {
-        if (!this._adaptiveEnabled)
-            return;
-
-        this._injectionsHandler.removeWithLabel('adaptive');
-        this._adaptiveEnabled = false;
-
-        // Once we removed the injection, we need to disconnect and
-        // reconnect all window signals.
-        for (let key of this._panel._trackedWindows.keys())
-            this._panel._trackedWindows.get(key).forEach(id => {
-                key.disconnect(id);
-            });
-
-        for (let win of this._panel._trackedWindows.keys())
-            this._panel._onWindowActorAdded(null, win);
     }
 };
 Signals.addSignalMethods(Transparency.prototype);

commit d4615acff92f90d1adb83e409b868bf453fef1c3

Fix regression with transparencey settings UI.
We were relying on the settings enum matching the combobox index. Since we
dropped the adaptive transparency mode, we need to explicitely map the combobox
entries via their id and refer to it when getting/setting the option.

Regression introduced by: 8ccc7d2d55e1b425be605ff3d29052f5a43994ec

Settings.ui
@@ -2162,9 +2162,9 @@
                                     <property name="can_focus">False</property>
                                     <property name="valign">center</property>
                                     <items>
-                                      <item translatable="yes">Default</item>
-                                      <item translatable="yes">Fixed</item>
-                                      <item translatable="yes">Dynamic</item>
+                                      <item id="0" translatable="yes">Default</item>
+                                      <item id="1" translatable="yes">Fixed</item>
+                                      <item id="3" translatable="yes">Dynamic</item>
                                     </items>
                                   </object>
                                   <packing>

prefs.js
@@ -751,13 +751,13 @@ var Settings = class DashToDock_Settings {
         });
 
         // Opacity
-        this._builder.get_object('customize_opacity_combo').set_active(
-            this._settings.get_enum('transparency-mode')
+        this._builder.get_object('customize_opacity_combo').set_active_id(
+            this._settings.get_enum('transparency-mode').toString()
         );
         this._builder.get_object('customize_opacity_combo').connect(
             'changed',
             (widget) => {
-                this._settings.set_enum('transparency-mode', widget.get_active());
+                this._settings.set_enum('transparency-mode', parseInt(widget.get_active_id()));
             }
         );
 