f7624842a07924bcdaabb26e8464c86a0f329e0c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-08-19 14:52:24 +0200

    locations: Ensure volume monitor is destroyed on destruction
    (cherry picked from commit 7b58ceb597eca8893018eba0275840b719a5e9d3)

--- locations.js
+++ locations.js
@@ -135,6 +135,7 @@ var Removables = class DashToDock_Removables {
 
     destroy() {
         this._signalsHandler.destroy();
+        this._monitor.run_dispose();
     }
 
     _getWorkingIconName(icon) {
