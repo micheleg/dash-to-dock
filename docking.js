micheleg/dash-to-dock

commit 662604b33f89f965e02664b63d1603be270e5c65

general: Use native clutter animations instead of Tweener
Upstream has stopped using Tweener in favor of native clutter animations.
So start use ease and transitions.

This also fixes icon label hiding

docking.js
@@ -16,7 +16,6 @@ const IconGrid = imports.ui.iconGrid;
 const Overview = imports.ui.overview;
 const OverviewControls = imports.ui.overviewControls;
 const PointerWatcher = imports.ui.pointerWatcher;
-const Tweener = imports.ui.tweener;
 const Signals = imports.signals;
 const ViewSelector = imports.ui.viewSelector;
 const WorkspaceSwitcherPopup= imports.ui.workspaceSwitcherPopup;
@@ -674,7 +673,7 @@ var DockedDash = GObject.registerClass({
     _show() {
         if ((this._dockState == State.HIDDEN) || (this._dockState == State.HIDING)) {
             if (this._dockState == State.HIDING)
-                // suppress all potential queued hiding animations - i.e. added to Tweener but not started,
+                // suppress all potential queued transitions - i.e. added but not started,
                 // always give priority to show
                 this._removeAnimations();
 
@@ -705,11 +704,10 @@ var DockedDash = GObject.registerClass({
     _animateIn(time, delay) {
         this._dockState = State.SHOWING;
 
-        Tweener.addTween(this._slider, {
-            slidex: 1,
-            time: time,
-            delay: delay,
-            transition: 'easeOutQuad',
+        this._slider.ease_property('slidex', 1, {
+            duration: time * 1000,
+            delay: delay * 1000,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
             onComplete: () => {
                 this._dockState = State.SHOWN;
                 // Remove barrier so that mouse pointer is released and can access monitors on other side of dock
@@ -724,11 +722,11 @@ var DockedDash = GObject.registerClass({
 
     _animateOut(time, delay) {
         this._dockState = State.HIDING;
-        Tweener.addTween(this._slider, {
-            slidex: 0,
-            time: time,
-            delay: delay ,
-            transition: 'easeOutQuad',
+
+        this._slider.ease_property('slidex', 0, {
+            duration: time * 1000,
+            delay: delay * 1000,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
             onComplete: () => {
                 this._dockState = State.HIDDEN;
                 // Remove queued barried removal if any
@@ -1102,7 +1100,7 @@ var DockedDash = GObject.registerClass({
     }
 
     _removeAnimations() {
-        Tweener.removeTweens(this._slider);
+        this._slider.remove_all_transitions();
     }
 
     _onDragStart() {

appIcons.js
@@ -25,7 +25,6 @@ const DND = imports.ui.dnd;
 const IconGrid = imports.ui.iconGrid;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
-const Tweener = imports.ui.tweener;
 const Util = imports.misc.util;
 const Workspace = imports.ui.workspace;
 
@@ -36,8 +35,6 @@ const WindowPreview = Me.imports.windowPreview;
 const AppIconIndicators = Me.imports.appIconIndicators;
 
 let tracker = Shell.WindowTracker.get_default();
-
-let DASH_ITEM_LABEL_SHOW_TIME = Dash.DASH_ITEM_LABEL_SHOW_TIME / 1000;
 
 const clickAction = {
     SKIP: 0,
@@ -1169,10 +1166,11 @@ function itemShowLabel()  {
     else if (x + labelWidth > monitor.x + monitor.width - gap)
         x -= x + labelWidth - (monitor.x + monitor.width) + gap;
 
+    this.label.remove_all_transitions();
     this.label.set_position(x, y);
-    Tweener.addTween(this.label, {
+    this.label.ease({
         opacity: 255,
-        time: DASH_ITEM_LABEL_SHOW_TIME / 1000,
-        transition: 'easeOutQuad',
+        duration: Dash.DASH_ITEM_LABEL_SHOW_TIME,
+        mode: Clutter.AnimationMode.EASE_OUT_QUAD
     });
 }

dash.js
@@ -18,7 +18,6 @@ const DND = imports.ui.dnd;
 const IconGrid = imports.ui.iconGrid;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
-const Tweener = imports.ui.tweener;
 const Util = imports.misc.util;
 const Workspace = imports.ui.workspace;
 
@@ -27,9 +26,9 @@ const Docking = Me.imports.docking;
 const Utils = Me.imports.utils;
 const AppIcons = Me.imports.appIcons;
 
-const DASH_ANIMATION_TIME = Dash.DASH_ANIMATION_TIME / 1000;
-const DASH_ITEM_LABEL_HIDE_TIME = Dash.DASH_ITEM_LABEL_HIDE_TIME / 1000;
-const DASH_ITEM_HOVER_TIMEOUT = Dash.DASH_ITEM_HOVER_TIMEOUT / 1000;
+const DASH_ANIMATION_TIME = Dash.DASH_ANIMATION_TIME;
+const DASH_ITEM_LABEL_HIDE_TIME = Dash.DASH_ITEM_LABEL_HIDE_TIME;
+const DASH_ITEM_HOVER_TIMEOUT = Dash.DASH_ITEM_HOVER_TIMEOUT;
 
 /**
  * Extend DashItemContainer
@@ -678,12 +677,13 @@ var MyDash = GObject.registerClass({
             icon.icon.set_size(icon.icon.width * scale,
                                icon.icon.height * scale);
 
-            Tweener.addTween(icon.icon,
-                             { width: targetWidth,
-                               height: targetHeight,
-                               time: DASH_ANIMATION_TIME,
-                               transition: 'easeOutQuad',
-                             });
+            icon.icon.remove_all_transitions();
+            icon.icon.ease({
+                width: targetWidth,
+                height: targetHeight,
+                time: DASH_ANIMATION_TIME,
+                mode: Clutter.AnimationMode.EASE_OUT_QUAD
+            });
         }
     }
 
@@ -1112,8 +1112,8 @@ function ensureActorVisibleInScrollView(scrollView, actor) {
     let adjust_v = true;
     let adjust_h = true;
 
-    let vadjustment = scrollView.vscroll.adjustment;
-    let hadjustment = scrollView.hscroll.adjustment;
+    let vadjustment = scrollView.get_vscroll_bar().get_adjustment();
+    let hadjustment = scrollView.get_hscroll_bar().get_adjustment();
     let [vvalue, vlower, vupper, vstepIncrement, vpageIncrement, vpageSize] = vadjustment.get_values();
     let [hvalue, hlower, hupper, hstepIncrement, hpageIncrement, hpageSize] = hadjustment.get_values();
 
@@ -1154,17 +1154,17 @@ function ensureActorVisibleInScrollView(scrollView, actor) {
         hvalue = Math.min(hupper - hpageSize, x2 + hoffset - hpageSize);
 
     if (vvalue !== vvalue0) {
-        Tweener.addTween(vadjustment, { value: vvalue,
-            time: Util.SCROLL_TIME / 1000,
-            transition: 'easeOutQuad'
+        vadjustment.ease(vvalue, {
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            duration: Util.SCROLL_TIME
         });
     }
 
     if (hvalue !== hvalue0) {
-        Tweener.addTween(hadjustment,
-                         { value: hvalue,
-                           time: Util.SCROLL_TIME / 1000,
-                           transition: 'easeOutQuad' });
+        hadjustment.ease(hvalue, {
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            duration: Util.SCROLL_TIME
+        });
     }
 
     return [hvalue- hvalue0, vvalue - vvalue0];

theming.js
@@ -17,7 +17,6 @@ const DND = imports.ui.dnd;
 const IconGrid = imports.ui.iconGrid;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
-const Tweener = imports.ui.tweener;
 const Util = imports.misc.util;
 const Workspace = imports.ui.workspace;
 

windowPreview.js
@@ -14,7 +14,6 @@ const Gtk = imports.gi.Gtk;
 
 const Params = imports.misc.params;
 const PopupMenu = imports.ui.popupMenu;
-const Tweener = imports.ui.tweener;
 const Workspace = imports.ui.workspace;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
@@ -22,6 +21,8 @@ const Utils = Me.imports.utils;
 
 const PREVIEW_MAX_WIDTH = 250;
 const PREVIEW_MAX_HEIGHT = 150;
+
+const PREVIEW_ANIMATION_DURATION = 250;
 
 var WindowPreviewMenu = class DashToDock_WindowPreviewMenu extends PopupMenu.PopupMenu {
 
@@ -413,6 +414,14 @@ class DashToDock_WindowPreviewMenuItem extends PopupMenu.PopupBaseMenuItem {
         this._clone = clone;
         this._mutterWindow = mutterWindow;
         this._cloneBin.set_child(this._clone);
+
+        this._clone.connect('destroy', () => {
+            if (this._destroyId) {
+                mutterWindow.disconnect(this._destroyId);
+                this._destroyId = 0;
+            }
+            this._clone = null;
+        })
     }
 
     _windowCanClose() {
@@ -499,18 +508,22 @@ class DashToDock_WindowPreviewMenuItem extends PopupMenu.PopupBaseMenuItem {
 
         if (this._windowCanClose()) {
             this.closeButton.show();
-            Tweener.addTween(this.closeButton,
-                             { opacity: 255,
-                               time: Workspace.WINDOW_OVERLAY_FADE_TIME / 1000,
-                               transition: 'easeOutQuad' });
+            this.closeButton.remove_all_transitions();
+            this.closeButton.ease({
+                opacity: 255,
+                duration: Workspace.WINDOW_OVERLAY_FADE_TIME,
+                mode: Clutter.AnimationMode.EASE_OUT_QUAD
+            });
         }
     }
 
     _hideCloseButton() {
-        Tweener.addTween(this.closeButton,
-                         { opacity: 0,
-                           time: Workspace.WINDOW_OVERLAY_FADE_TIME / 1000,
-                           transition: 'easeInQuad' });
+        this.closeButton.remove_all_transitions();
+        this.closeButton.ease({
+            opacity: 0,
+            duration: Workspace.WINDOW_OVERLAY_FADE_TIME,
+            mode: Clutter.AnimationMode.EASE_IN_QUAD
+        });
     }
 
     show(animate) {
@@ -519,31 +532,30 @@ class DashToDock_WindowPreviewMenuItem extends PopupMenu.PopupBaseMenuItem {
         this.opacity = 0;
         this.set_width(0);
 
-        let time = animate ? 0.25 : 0;
-        Tweener.addTween(this,
-                         { opacity: 255,
-                           width: fullWidth,
-                           time: time,
-                           transition: 'easeInOutQuad'
-                         });
+        let time = animate ? PREVIEW_ANIMATION_DURATION : 0;
+        this.remove_all_transitions();
+        this.ease({
+            opacity: 255,
+            width: fullWidth,
+            duration: time,
+            mode: Clutter.AnimationMode.EASE_IN_OUT_QUAD,
+        });
     }
 
     _animateOutAndDestroy() {
-        Tweener.addTween(this,
-                         { opacity: 0,
-                           time: 0.25,
-                         });
+        this.remove_all_transitions();
+        this.ease({
+            opacity: 0,
+            duration: PREVIEW_ANIMATION_DURATION,
+        });
 
-        Tweener.addTween(this,
-                         { height: 0,
-                           width: 0,
-                           time: 0.25,
-                           delay: 0.25,
-                           onCompleteScope: this,
-                           onComplete() {
-                              this.destroy();
-                           }
-                         });
+        this.ease({
+            width: 0,
+            height: 0,
+            duration: PREVIEW_ANIMATION_DURATION,
+            delay: PREVIEW_ANIMATION_DURATION,
+            onComplete: () => this.destroy()
+        });
     }
 
     activate() {

commit 5e815367294d75ec886e497750d0345c551e0fbd

docking: Delay hide requests if we're currently showing
Wait that the show animation is completed before we try to hide a sliding
dock.

This fixes a regression caused by commit 662604b because with clutter
animations, a transition would remove the previous one queued for an object,
and thus the delay wouldn't work anymore.

So, instead of relying on the waiting time, let's just use the _animateIn()
onComplete() callback in order to request an hide while the show animation
is still going.

docking.js
@@ -681,15 +681,14 @@ var DockedDash = GObject.registerClass({
         // If no hiding animation is running or queued
         if ((this._dockState == State.SHOWN) || (this._dockState == State.SHOWING)) {
             let settings = DockManager.settings;
-            let delay;
+            let delay = settings.get_double('hide-delay');
 
-            if (this._dockState == State.SHOWING)
-                //if a show already started, let it finish; queue hide without removing the show.
-                // to obtain this I increase the delay to avoid the overlap and interference
-                // between the animations
-                delay = settings.get_double('hide-delay') + settings.get_double('animation-time');
-            else
-                delay = settings.get_double('hide-delay');
+            if (this._dockState == State.SHOWING) {
+                // if a show already started, let it finish; queue hide without removing the show.
+                // to obtain this, we wait for the animateIn animation to be completed
+                this._delayedHide = true;
+                return;
+            }
 
             this.emit('hiding');
             this._animateOut(settings.get_double('animation-time'), delay);
@@ -698,6 +697,7 @@ var DockedDash = GObject.registerClass({
 
     _animateIn(time, delay) {
         this._dockState = State.SHOWING;
+        delete this._delayedHide;
 
         this._slider.ease_property('slidex', 1, {
             duration: time * 1000,
@@ -710,8 +710,13 @@ var DockedDash = GObject.registerClass({
                 // gives users an opportunity to hover over the dock
                 if (this._removeBarrierTimeoutId > 0)
                     GLib.source_remove(this._removeBarrierTimeoutId);
-                this._removeBarrierTimeoutId = GLib.timeout_add(
-                    GLib.PRIORITY_DEFAULT, 100, this._removeBarrier.bind(this));
+
+                if (!this._delayedHide) {
+                    this._removeBarrierTimeoutId = GLib.timeout_add(
+                        GLib.PRIORITY_DEFAULT, 100, this._removeBarrier.bind(this));
+                } else {
+                    this._hide();
+                }
             }
         });
     }