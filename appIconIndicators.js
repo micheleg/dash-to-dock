micheleg/dash-to-dock

commit 946ecbe83215c037d1b637c135ddd181f409bcd0

appIcons: Move logic for computing the focused / running state here
This is used to be handled by the running indicators, but there's no
point to do this while we can manage it nicely with implicit properties
now.

So let's add focused, runnning and windows-count properties to
MyAppIcon and connect to these changes in the indicators side, so that
we can track it

appIconIndicators.js
@@ -142,57 +142,30 @@ var RunningIndicatorBase = class DashToDock_RunningIndicatorBase extends Indicat
         super(source)
 
         this._side = Utils.getPosition();
-        this._nWindows = 0;
-
         this._dominantColorExtractor = new DominantColorExtractor(this._source.app);
-
-        // These statuses take into account the workspace/monitor isolation
-        this._isFocused = false;
-        this._isRunning = false;
+        this._signalsHandler.add(this._source, 'notify::running', () => this.update());
+        this._signalsHandler.add(this._source, 'notify::focused', () => this.update());
+        this._signalsHandler.add(this._source, 'notify::windows-count', () => this._updateCounterClass());
+        this.update();
     }
 
     update() {
-        // Limit to 1 to MAX_WINDOWS_CLASSES  windows classes
-        this._nWindows = Math.min(this._source.getInterestingWindows().length, MAX_WINDOWS_CLASSES);
-
-        // We need to check the number of windows, as the focus might be
-        // happening on another monitor if using isolation
-        if (tracker.focus_app == this._source.app && this._nWindows > 0)
-            this._isFocused = true;
-        else
-            this._isFocused = false;
-
-        // In the case of workspace isolation, we need to hide the dots of apps with
-        // no windows in the current workspace
-        if ((this._source.app.state != Shell.AppState.STOPPED || this._source.isLocation()) && this._nWindows > 0)
-            this._isRunning = true;
-        else
-            this._isRunning = false;
-
         this._updateCounterClass();
-        this._updateFocusClass();
         this._updateDefaultDot();
     }
 
     _updateCounterClass() {
         for (let i = 1; i <= MAX_WINDOWS_CLASSES; i++) {
             let className = 'running' + i;
-            if (i != this._nWindows)
+            if (i != this._source.windowsCount)
                 this._source.remove_style_class_name(className);
             else
                 this._source.add_style_class_name(className);
         }
     }
 
-    _updateFocusClass() {
-        if (this._isFocused)
-            this._source.add_style_class_name('focused');
-        else
-            this._source.remove_style_class_name('focused');
-    }
-
     _updateDefaultDot() {
-        if (this._isRunning)
+        if (this._source.running)
             this._source._dot.show();
         else
             this._source._dot.hide();
@@ -335,7 +308,7 @@ var RunningIndicatorDots = class DashToDock_RunningIndicatorDots extends Running
         if (!Docking.DockManager.settings.get_boolean('apply-custom-theme') &&
             Docking.DockManager.settings.get_boolean('unity-backlit-items')) {
             this._source._iconContainer.get_children()[1].set_style(this._glossyBackgroundStyle);
-            if (this._isRunning)
+            if (this._source.running)
                 this._enableBacklight();
             else
                 this._disableBacklight();
@@ -416,7 +389,7 @@ var RunningIndicatorDots = class DashToDock_RunningIndicatorDots extends Running
 
     _drawIndicator(cr) {
         // Draw the required numbers of dots
-        let n = this._nWindows;
+        let n = this._source.windowsCount;
 
         cr.setLineWidth(this._borderWidth);
         Clutter.cairo_set_source_color(cr, this._borderColor);
@@ -444,11 +417,10 @@ var RunningIndicatorDots = class DashToDock_RunningIndicatorDots extends Running
 var RunningIndicatorCiliora = class DashToDock_RunningIndicatorCiliora extends RunningIndicatorDots {
 
     _drawIndicator(cr) {
-        if (this._isRunning) {
-
+        if (this._source.running) {
             let size =  Math.max(this._width/20, this._borderWidth);
             let spacing = size; // separation between the dots
-            let lineLength = this._width - (size*(this._nWindows-1)) - (spacing*(this._nWindows-1));
+            let lineLength = this._width - (size*(this._source.windowsCount-1)) - (spacing*(this._source.windowsCount-1));
             let padding = this._borderWidth;
             // For the backlit case here we don't want the outer border visible
             if (Docking.DockManager.settings.get_boolean('unity-backlit-items') &&
@@ -462,7 +434,7 @@ var RunningIndicatorCiliora = class DashToDock_RunningIndicatorCiliora extends R
             cr.translate(0, yOffset);
             cr.newSubPath();
             cr.rectangle(0, 0, lineLength, size);
-            for (let i = 1; i < this._nWindows; i++) {
+            for (let i = 1; i < this._source.windowsCount; i++) {
                 cr.newSubPath();
                 cr.rectangle(lineLength + (i*spacing) + ((i-1)*size), 0, size, size);
             }
@@ -479,11 +451,11 @@ var RunningIndicatorCiliora = class DashToDock_RunningIndicatorCiliora extends R
 var RunningIndicatorSegmented = class DashToDock_RunningIndicatorSegmented extends RunningIndicatorDots {
 
     _drawIndicator(cr) {
-        if (this._isRunning) {
+        if (this._source.running) {
             let size =  Math.max(this._width/20, this._borderWidth);
             let spacing = Math.ceil(this._width/18); // separation between the dots
-            let dashLength = Math.ceil((this._width - ((this._nWindows-1)*spacing))/this._nWindows);
-            let lineLength = this._width - (size*(this._nWindows-1)) - (spacing*(this._nWindows-1));
+            let dashLength = Math.ceil((this._width - ((this._source.windowsCount-1)*spacing))/this._source.windowsCount);
+            let lineLength = this._width - (size*(this._source.windowsCount-1)) - (spacing*(this._source.windowsCount-1));
             let padding = this._borderWidth;
             // For the backlit case here we don't want the outer border visible
             if (Docking.DockManager.settings.get_boolean('unity-backlit-items') &&
@@ -495,7 +467,7 @@ var RunningIndicatorSegmented = class DashToDock_RunningIndicatorSegmented exten
             Clutter.cairo_set_source_color(cr, this._borderColor);
 
             cr.translate(0, yOffset);
-            for (let i = 0; i < this._nWindows; i++) {
+            for (let i = 0; i < this._source.windowsCount; i++) {
                 cr.newSubPath();
                 cr.rectangle(i*dashLength + i*spacing, 0, dashLength, size);
             }
@@ -512,8 +484,7 @@ var RunningIndicatorSegmented = class DashToDock_RunningIndicatorSegmented exten
 var RunningIndicatorSolid = class DashToDock_RunningIndicatorSolid extends RunningIndicatorDots {
 
     _drawIndicator(cr) {
-        if (this._isRunning) {
-
+        if (this._source.running) {
             let size =  Math.max(this._width/20, this._borderWidth);
             let padding = this._borderWidth;
             // For the backlit case here we don't want the outer border visible
@@ -532,7 +503,6 @@ var RunningIndicatorSolid = class DashToDock_RunningIndicatorSolid extends Runni
             cr.strokePreserve();
             Clutter.cairo_set_source_color(cr, this._bodyColor);
             cr.fill();
-
         }
     }
 };
@@ -542,7 +512,7 @@ var RunningIndicatorSolid = class DashToDock_RunningIndicatorSolid extends Runni
 var RunningIndicatorSquares = class DashToDock_RunningIndicatorSquares extends RunningIndicatorDots {
 
     _drawIndicator(cr) {
-        if (this._isRunning) {
+        if (this._source.running) {
             let size =  Math.max(this._width/11, this._borderWidth);
             let padding = this._borderWidth;
             let spacing = Math.ceil(this._width/18); // separation between the dots
@@ -551,8 +521,8 @@ var RunningIndicatorSquares = class DashToDock_RunningIndicatorSquares extends R
             cr.setLineWidth(this._borderWidth);
             Clutter.cairo_set_source_color(cr, this._borderColor);
 
-            cr.translate(Math.floor((this._width - this._nWindows*size - (this._nWindows-1)*spacing)/2), yOffset);
-            for (let i = 0; i < this._nWindows; i++) {
+            cr.translate(Math.floor((this._width - this._source.windowsCount*size - (this._source.windowsCount-1)*spacing)/2), yOffset);
+            for (let i = 0; i < this._source.windowsCount; i++) {
                 cr.newSubPath();
                 cr.rectangle(i*size + i*spacing, 0, size, size);
             }
@@ -568,7 +538,7 @@ var RunningIndicatorSquares = class DashToDock_RunningIndicatorSquares extends R
 var RunningIndicatorDashes = class DashToDock_RunningIndicatorDashes extends RunningIndicatorDots {
 
     _drawIndicator(cr) {
-        if (this._isRunning) {
+        if (this._source.running) {
             let size =  Math.max(this._width/20, this._borderWidth);
             let padding = this._borderWidth;
             let spacing = Math.ceil(this._width/18); // separation between the dots
@@ -578,8 +548,8 @@ var RunningIndicatorDashes = class DashToDock_RunningIndicatorDashes extends Run
             cr.setLineWidth(this._borderWidth);
             Clutter.cairo_set_source_color(cr, this._borderColor);
 
-            cr.translate(Math.floor((this._width - this._nWindows*dashLength - (this._nWindows-1)*spacing)/2), yOffset);
-            for (let i = 0; i < this._nWindows; i++) {
+            cr.translate(Math.floor((this._width - this._source.windowsCount*dashLength - (this._source.windowsCount-1)*spacing)/2), yOffset);
+            for (let i = 0; i < this._source.windowsCount; i++) {
                 cr.newSubPath();
                 cr.rectangle(i*dashLength + i*spacing, 0, dashLength, size);
             }
@@ -606,7 +576,7 @@ var RunningIndicatorMetro = class DashToDock_RunningIndicatorMetro extends Runni
     }
 
     _drawIndicator(cr) {
-        if (this._isRunning) {
+        if (this._source.running) {
             let size =  Math.max(this._width/20, this._borderWidth);
             let padding = 0;
             // For the backlit case here we don't want the outer border visible
@@ -615,7 +585,7 @@ var RunningIndicatorMetro = class DashToDock_RunningIndicatorMetro extends Runni
                 padding = 0;
             let yOffset = this._height - padding - size;
 
-            let n = this._nWindows;
+            let n = this._source.windowsCount;
             if(n <= 1) {
                 cr.translate(0, yOffset);
                 Clutter.cairo_set_source_color(cr, this._bodyColor);
@@ -624,7 +594,7 @@ var RunningIndicatorMetro = class DashToDock_RunningIndicatorMetro extends Runni
                 cr.fill();
             } else {
                 let blackenedLength = (1/48)*this._width; // need to scale with the SVG for the stacked highlight
-                let darkenedLength = this._isFocused ? (2/48)*this._width : (10/48)*this._width;
+                let darkenedLength = this._source.focused ? (2/48)*this._width : (10/48)*this._width;
                 let blackenedColor = this._bodyColor.shade(.3);
                 let darkenedColor = this._bodyColor.shade(.7);
 

appIcons.js
@@ -73,8 +73,22 @@ let recentlyClickedAppMonitor = -1;
  * - Update minimization animation target
  * - Update menu if open on windows change
  */
-var MyAppIcon = GObject.registerClass(
-class MyAppIcon extends Dash.DashIcon {
+var MyAppIcon = GObject.registerClass({
+    Properties: {
+        'focused': GObject.ParamSpec.boolean(
+            'focused', 'focused', 'focused',
+            GObject.ParamFlags.READWRITE,
+            false),
+        'running': GObject.ParamSpec.boolean(
+            'running', 'running', 'running',
+            GObject.ParamFlags.READWRITE,
+            false),
+        'windows-count': GObject.ParamSpec.uint(
+            'windows-count', 'windows-count', 'windows-count',
+            GObject.ParamFlags.READWRITE,
+            0, GLib.MAXUINT32, 0),
+    }
+}, class MyAppIcon extends Dash.DashIcon {
     // settings are required inside.
     _init(remoteModel, app, monitorIndex, iconAnimator) {
         super._init(app);
@@ -84,12 +98,10 @@ class MyAppIcon extends Dash.DashIcon {
         this._signalsHandler = new Utils.GlobalSignalsHandler(this);
         this.remoteModel = remoteModel;
         this.iconAnimator = iconAnimator;
-        this._indicator = null;
+        this._indicator = new AppIconIndicators.AppIconIndicator(this);
 
         let appInfo = app.get_app_info();
         this._location = appInfo ? appInfo.get_string('XdtdUri') : null;
-
-        this._updateIndicatorStyle();
 
         // Monitor windows-changes instead of app state.
         // Keep using the same Id and function callback (that is extended)
@@ -98,10 +110,9 @@ class MyAppIcon extends Dash.DashIcon {
             this._stateChangedId = 0;
         }
 
-        this._windowsChangedId = this.app.connect('windows-changed',
-                                                  this.onWindowsChanged.bind(this));
-        this._focusAppChangeId = tracker.connect('notify::focus-app',
-                                                 this._onFocusAppChanged.bind(this));
+        this._signalsHandler.add(this.app, 'windows-changed', () => this.onWindowsChanged());
+        this._signalsHandler.add(this.app, 'notify::state', () => this._updateRunningState());
+        this._signalsHandler.add(tracker, 'notify::focus-app', () => this._updateFocusState());
 
         // In Wayland sessions, this signal is needed to track the state of windows dragged
         // from one monitor to another. As this is triggered quite often (whenever a new winow
@@ -124,13 +135,15 @@ class MyAppIcon extends Dash.DashIcon {
                    'running-indicator-style',
                     ];
 
-        keys.forEach(function(key) {
+        keys.forEach(key => {
             this._signalsHandler.add(
                 Docking.DockManager.settings,
-                'changed::' + key,
-                this._updateIndicatorStyle.bind(this)
+                'changed::' + key, () => {
+                    this._indicator.destroy();
+                    this._indicator = new AppIconIndicators.AppIconIndicator(this);
+                }
             );
-        }, this);
+        });
 
         if (this._location) {
             this._signalsHandler.add(
@@ -140,6 +153,7 @@ class MyAppIcon extends Dash.DashIcon {
             );
         }
 
+        this._updateState();
         this._numberOverlay();
 
         this._previewMenuManager = null;
@@ -154,28 +168,6 @@ class MyAppIcon extends Dash.DashIcon {
         // It can be safely removed once it get solved upstrea.
         if (this._menu)
             this._menu.close(false);
-
-        // Disconect global signals
-
-        if (this._windowsChangedId > 0)
-            this.app.disconnect(this._windowsChangedId);
-        this._windowsChangedId = 0;
-
-        if (this._focusAppChangeId > 0) {
-            tracker.disconnect(this._focusAppChangeId);
-            this._focusAppChangeId = 0;
-        }
-    }
-
-    // TOOD Rename this function
-    _updateIndicatorStyle() {
-
-        if (this._indicator !== null) {
-            this._indicator.destroy();
-            this._indicator = null;
-        }
-        this._indicator = new AppIconIndicators.AppIconIndicator(this);
-        this._indicator.update();
     }
 
     _onWindowEntered(metaScreen, monitorIndex, metaWin) {
@@ -193,10 +185,7 @@ class MyAppIcon extends Dash.DashIcon {
         // We only activate windows of running applications, i.e. we never open new windows
         // We check if the app is running, and that the # of windows is > 0 in
         // case we use workspace isolation,
-        let appIsRunning = this.app.state == Shell.AppState.RUNNING
-            && this.getInterestingWindows().length > 0;
-
-        if (!appIsRunning)
+        if (!this.running)
             return Clutter.EVENT_PROPAGATE;
 
         if (this._optionalScrollCycleWindowsDeadTimeId)
@@ -225,10 +214,9 @@ class MyAppIcon extends Dash.DashIcon {
             break;
         }
 
-        let focusedApp = tracker.focus_app;
         if (!Main.overview._shown) {
             let reversed = direction === Meta.MotionDirection.UP;
-            if (this.app == focusedApp)
+            if (this.focused)
                 this._cycleThroughWindows(reversed);
             else {
                 // Activate the first window
@@ -245,12 +233,36 @@ class MyAppIcon extends Dash.DashIcon {
     }
 
     onWindowsChanged() {
-
         if (this._menu && this._menu.isOpen)
             this._menu.update();
 
-        this._indicator.update();
+        this._updateState();
         this.updateIconGeometry();
+    }
+
+    _updateState() {
+        this.windowsCount = this.getInterestingWindows().length;
+        this._updateRunningState();
+        this._updateFocusState();
+    }
+
+    _updateRunningState() {
+        this.running = (this.app.state === Shell.AppState.RUNNING || this.isLocation()) &&
+            this.windowsCount;
+
+        if (this.running)
+            this.add_style_class_name('running');
+        else
+            this.remove_style_class_name('running');
+    }
+
+    _updateFocusState() {
+        this.focused = (tracker.focus_app === this.app && this.running);
+
+        if (this.focused)
+            this.add_style_class_name('focused');
+        else
+            this.remove_style_class_name('focused');
     }
 
     /**
@@ -337,14 +349,9 @@ class MyAppIcon extends Dash.DashIcon {
         return false;
     }
 
-    _onFocusAppChanged() {
-        this._indicator.update();
-    }
-
     activate(button) {
         let event = Clutter.get_current_event();
         let modifiers = event ? event.get_state() : 0;
-        let focusedApp = tracker.focus_app;
 
         // Only consider SHIFT and CONTROL as modifiers (exclude SUPER, CAPS-LOCK, etc.)
         modifiers = modifiers & (Clutter.ModifierType.SHIFT_MASK | Clutter.ModifierType.CONTROL_MASK);
@@ -380,15 +387,13 @@ class MyAppIcon extends Dash.DashIcon {
         // We check if the app is running, and that the # of windows is > 0 in
         // case we use workspace isolation.
         let windows = this.getInterestingWindows();
-        let appIsRunning = (this.app.state == Shell.AppState.RUNNING || this.isLocation())
-            && windows.length > 0;
 
         // Some action modes (e.g. MINIMIZE_OR_OVERVIEW) require overview to remain open
         // This variable keeps track of this
         let shouldHideOverview = true;
 
         // We customize the action only when the application is already running
-        if (appIsRunning) {
+        if (this.running) {
             switch (buttonAction) {
             case clickAction.MINIMIZE:
                 // In overview just activate the app, unless the acion is explicitely
@@ -396,7 +401,7 @@ class MyAppIcon extends Dash.DashIcon {
                 if (!Main.overview._shown || modifiers){
                     // If we have button=2 or a modifier, allow minimization even if
                     // the app is not focused
-                    if (this.app == focusedApp || button == 2 || modifiers & Clutter.ModifierType.SHIFT_MASK) {
+                    if (this.focused || button === 2 || modifiers & Clutter.ModifierType.SHIFT_MASK) {
                         // minimize all windows on double click and always in the case of primary click without
                         // additional modifiers
                         let click_count = 0;
@@ -420,7 +425,7 @@ class MyAppIcon extends Dash.DashIcon {
                 // simple click action (no modifiers, no middle click).
                 if (windows.length == 1 && !modifiers && button == 1) {
                     let w = windows[0];
-                    if (this.app == focusedApp) {
+                    if (this.focused) {
                         // Window is raised, minimize it
                         this._minimizeWindow(w);
                     } else {
@@ -437,7 +442,7 @@ class MyAppIcon extends Dash.DashIcon {
 
             case clickAction.CYCLE_WINDOWS:
                 if (!Main.overview._shown){
-                    if (this.app == focusedApp)
+                    if (this.focused)
                         this._cycleThroughWindows();
                     else {
                         // Activate the first window
@@ -450,7 +455,7 @@ class MyAppIcon extends Dash.DashIcon {
                 break;
 
             case clickAction.FOCUS_OR_PREVIEWS:
-                if (this.app == focusedApp &&
+                if (this.focused &&
                     (windows.length > 1 || modifiers || button != 1)) {
                     this._windowPreviews();
                 } else {
@@ -461,7 +466,7 @@ class MyAppIcon extends Dash.DashIcon {
                 break;
 
             case clickAction.FOCUS_MINIMIZE_OR_PREVIEWS:
-                if (this.app == focusedApp) {
+                if (this.focused) {
                     if (windows.length > 1 || modifiers || button != 1)
                         this._windowPreviews();
                     else if (!Main.overview.visible)
@@ -499,7 +504,7 @@ class MyAppIcon extends Dash.DashIcon {
                 if (!Main.overview._shown){
                     if (windows.length == 1 && !modifiers && button == 1) {
                         let w = windows[0];
-                        if (this.app == focusedApp) {
+                        if (this.focused) {
                             // Window is raised, minimize it
                             this._minimizeWindow(w);
                         } else {
@@ -950,16 +955,13 @@ const MyAppIconMenu = class DashToDock_MyAppIconMenu extends AppDisplay.AppIconM
     // update menu content when application windows change. This is desirable as actions
     // acting on windows (closing) are performed while the menu is shown.
     update() {
-
-      let windows = this._source.getInterestingWindows();
-
       // update, show or hide the quit menu
-      if ( windows.length > 0) {
+      if (this._source.windowsCount > 0) {
           let quitFromDashMenuText = "";
-          if (windows.length == 1)
+          if (this._source.windowsCount == 1)
               this._quitfromDashMenuItem.label.set_text(_('Quit'));
           else
-              this._quitfromDashMenuItem.label.set_text(__('Quit %d Windows').format(windows.length));
+              this._quitfromDashMenuItem.label.set_text(__('Quit %d Windows').format(this._source.windowsCount));
 
           this._quitfromDashMenuItem.actor.show();
 
@@ -967,7 +969,8 @@ const MyAppIconMenu = class DashToDock_MyAppIconMenu extends AppDisplay.AppIconM
           this._quitfromDashMenuItem.actor.hide();
       }
 
-      if(Docking.DockManager.settings.get_boolean('show-windows-preview')){
+      if (Docking.DockManager.settings.get_boolean('show-windows-preview')){
+          const windows = this._source.getInterestingWindows();
 
           // update, show, or hide the allWindows menu
           // Check if there are new windows not already displayed. In such case, repopulate the allWindows

commit 101b8194ba0a089b70e7839415a53088f4daa049

appIconIndicators: Limit the number of running indicators to 4
This was already the default, but got broken with commit 946ecbe83 as we
were just always using the windows number.

appIconIndicators.js
@@ -149,6 +149,10 @@ var RunningIndicatorBase = class DashToDock_RunningIndicatorBase extends Indicat
         this.update();
     }
 
+    get _number() {
+        return Math.min(this._source.windowsCount, MAX_WINDOWS_CLASSES);
+    }
+
     update() {
         this._updateCounterClass();
         this._updateDefaultDot();
@@ -157,7 +161,7 @@ var RunningIndicatorBase = class DashToDock_RunningIndicatorBase extends Indicat
     _updateCounterClass() {
         for (let i = 1; i <= MAX_WINDOWS_CLASSES; i++) {
             let className = 'running' + i;
-            if (i != this._source.windowsCount)
+            if (i != this._number)
                 this._source.remove_style_class_name(className);
             else
                 this._source.add_style_class_name(className);
@@ -389,7 +393,7 @@ var RunningIndicatorDots = class DashToDock_RunningIndicatorDots extends Running
 
     _drawIndicator(cr) {
         // Draw the required numbers of dots
-        let n = this._source.windowsCount;
+        let n = this._number;
 
         cr.setLineWidth(this._borderWidth);
         Clutter.cairo_set_source_color(cr, this._borderColor);
@@ -420,7 +424,7 @@ var RunningIndicatorCiliora = class DashToDock_RunningIndicatorCiliora extends R
         if (this._source.running) {
             let size =  Math.max(this._width/20, this._borderWidth);
             let spacing = size; // separation between the dots
-            let lineLength = this._width - (size*(this._source.windowsCount-1)) - (spacing*(this._source.windowsCount-1));
+            let lineLength = this._width - (size*(this._number-1)) - (spacing*(this._number-1));
             let padding = this._borderWidth;
             // For the backlit case here we don't want the outer border visible
             if (Docking.DockManager.settings.get_boolean('unity-backlit-items') &&
@@ -434,7 +438,7 @@ var RunningIndicatorCiliora = class DashToDock_RunningIndicatorCiliora extends R
             cr.translate(0, yOffset);
             cr.newSubPath();
             cr.rectangle(0, 0, lineLength, size);
-            for (let i = 1; i < this._source.windowsCount; i++) {
+            for (let i = 1; i < this._number; i++) {
                 cr.newSubPath();
                 cr.rectangle(lineLength + (i*spacing) + ((i-1)*size), 0, size, size);
             }
@@ -454,8 +458,8 @@ var RunningIndicatorSegmented = class DashToDock_RunningIndicatorSegmented exten
         if (this._source.running) {
             let size =  Math.max(this._width/20, this._borderWidth);
             let spacing = Math.ceil(this._width/18); // separation between the dots
-            let dashLength = Math.ceil((this._width - ((this._source.windowsCount-1)*spacing))/this._source.windowsCount);
-            let lineLength = this._width - (size*(this._source.windowsCount-1)) - (spacing*(this._source.windowsCount-1));
+            let dashLength = Math.ceil((this._width - ((this._number-1)*spacing))/this._number);
+            let lineLength = this._width - (size*(this._number-1)) - (spacing*(this._number-1));
             let padding = this._borderWidth;
             // For the backlit case here we don't want the outer border visible
             if (Docking.DockManager.settings.get_boolean('unity-backlit-items') &&
@@ -467,7 +471,7 @@ var RunningIndicatorSegmented = class DashToDock_RunningIndicatorSegmented exten
             Clutter.cairo_set_source_color(cr, this._borderColor);
 
             cr.translate(0, yOffset);
-            for (let i = 0; i < this._source.windowsCount; i++) {
+            for (let i = 0; i < this._number; i++) {
                 cr.newSubPath();
                 cr.rectangle(i*dashLength + i*spacing, 0, dashLength, size);
             }
@@ -521,8 +525,8 @@ var RunningIndicatorSquares = class DashToDock_RunningIndicatorSquares extends R
             cr.setLineWidth(this._borderWidth);
             Clutter.cairo_set_source_color(cr, this._borderColor);
 
-            cr.translate(Math.floor((this._width - this._source.windowsCount*size - (this._source.windowsCount-1)*spacing)/2), yOffset);
-            for (let i = 0; i < this._source.windowsCount; i++) {
+            cr.translate(Math.floor((this._width - this._number*size - (this._number-1)*spacing)/2), yOffset);
+            for (let i = 0; i < this._number; i++) {
                 cr.newSubPath();
                 cr.rectangle(i*size + i*spacing, 0, size, size);
             }
@@ -548,8 +552,8 @@ var RunningIndicatorDashes = class DashToDock_RunningIndicatorDashes extends Run
             cr.setLineWidth(this._borderWidth);
             Clutter.cairo_set_source_color(cr, this._borderColor);
 
-            cr.translate(Math.floor((this._width - this._source.windowsCount*dashLength - (this._source.windowsCount-1)*spacing)/2), yOffset);
-            for (let i = 0; i < this._source.windowsCount; i++) {
+            cr.translate(Math.floor((this._width - this._number*dashLength - (this._number-1)*spacing)/2), yOffset);
+            for (let i = 0; i < this._number; i++) {
                 cr.newSubPath();
                 cr.rectangle(i*dashLength + i*spacing, 0, dashLength, size);
             }
@@ -585,7 +589,7 @@ var RunningIndicatorMetro = class DashToDock_RunningIndicatorMetro extends Runni
                 padding = 0;
             let yOffset = this._height - padding - size;
 
-            let n = this._source.windowsCount;
+            let n = this._number;
             if(n <= 1) {
                 cr.translate(0, yOffset);
                 Clutter.cairo_set_source_color(cr, this._bodyColor);