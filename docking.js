0e579921f75b6219053ed3acf48c2aa5eba246f4
nename0 <26363498+nename0@users.noreply.github.com>
2021-11-06 22:35:19 +0100

    Cleanup nautilus listeners

--- docking.js
+++ docking.js
@@ -2226,6 +2226,7 @@ var DockManager = class DashToDock_DockManager {
         }
         this._trash?.destroy();
         this._trash = null;
+        Locations.unWrapWindowsManagerApp();
         this._removables?.destroy();
         this._removables = null;
         this._iconTheme.destroy();
