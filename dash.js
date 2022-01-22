283cc5da5e9f8db455165491056e2a5a1b32bf04
Marco Trevisan (Treviño) <mail@3v1n0.net>
2022-01-22 03:34:16 +0100

    Dash: Update appIcon geometry on parent geometry changes
    We used to compute the app icon geometry when an icon position was not
    set yet, leading to windows minimizing to the top-left corner.
    
    Instead of handling this when resetting the dash icons, now just monitor
    the appIcon container (that is the one that is actually moving around in
    the launcher) geometry and update the icons geometry on its changes.
    
    Fixes: #1567

--- dash.js
+++ dash.js
@@ -536,6 +536,9 @@ var DockDash = GObject.registerClass({
         appIcon.icon.setIconSize(this.iconSize);
         this._hookUpLabel(item, appIcon);
 
+        item.connect('notify::position', () => appIcon.updateIconGeometry());
+        item.connect('notify::size', () => appIcon.updateIconGeometry());
+
         return item;
     }
 
@@ -571,13 +574,6 @@ var DockDash = GObject.registerClass({
         });
 
       return appIcons;
-    }
-
-    _updateAppsIconGeometry() {
-        let appIcons = this.getAppIcons();
-        appIcons.forEach(function(icon) {
-            icon.updateIconGeometry();
-        });
     }
 
     _itemMenuStateChanged(item, opened) {
@@ -927,9 +923,6 @@ var DockDash = GObject.registerClass({
         // Workaround for https://bugzilla.gnome.org/show_bug.cgi?id=692744
         // Without it, StBoxLayout may use a stale size cache
         this._box.queue_relayout();
-        // TODO
-        // This is required for icon reordering when the scrollview is used.
-        this._updateAppsIconGeometry();
 
         // This will update the size, and the corresponding number for each icon
         this._updateNumberOverlay();
