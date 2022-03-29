afea4a4976a75809f47211e1f6fbbbfa892fc6d2
Jeremy Soller <jackpot51@gmail.com>
2022-03-29 12:40:16 -0600

    Implement manual hide

--- docking.js
+++ docking.js
@@ -559,6 +559,12 @@ var DockedDash = GObject.registerClass({
             }
         ], [
             settings,
+            'changed::manualhide',
+            () => {
+                this._updateVisibilityMode();
+            }
+        ], [
+            settings,
             'changed::intellihide',
             () => {
                 this._updateVisibilityMode();
@@ -608,7 +614,7 @@ var DockedDash = GObject.registerClass({
      */
     _updateVisibilityMode() {
         let settings = DockManager.settings;
-        if (DockManager.settings.dockFixed) {
+        if (DockManager.settings.dockFixed || DockManager.settings.manualhide) {
             this._autohideIsEnabled = false;
             this._intellihideIsEnabled = false;
         }
@@ -639,6 +645,12 @@ var DockedDash = GObject.registerClass({
      * overview visibility
      */
     _updateDashVisibility() {
+        if (DockManager.settings.manualhide) {
+            this._ignoreHover = false;
+            this._animateOut(DockManager.settings.animationTime, 0);
+            return;
+        }
+
         if (Main.overview.visibleTarget)
             return;
 
