7e7add84c232348e2be74209956879abeeffe052
13r0ck <bnr@tuta.io>
2022-05-05 16:27:06 -0600

    Add default-windows-preivew-to-open
    Adds a gsetting to open windows preview when an app icon is right clicked.
    Defaults to false (off) which is the existing behaviour.

--- appIcons.js
+++ appIcons.js
@@ -1154,6 +1154,9 @@ const DockAppIconMenu = class DockAppIconMenu extends PopupMenu.PopupMenu {
           if (windows.length > 0){
               this._allWindowsMenuItem.show();
               this._allWindowsMenuItem.setSensitive(true);
+
+              if (Docking.DockManager.settings.defaultWindowsPreviewToOpen)
+                  this._allWindowsMenuItem.menu.open();
           }
       }
 
