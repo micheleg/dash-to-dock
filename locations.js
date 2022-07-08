a63dc9f615767f947aabd02643d747fca69dba79
Marco Trevisan (Treviño) <mail@3v1n0.net>
2022-07-08 05:19:31 +0200

    locations: Do not keep references on the Trash cancellable
    It's already managed by the AppInfo, so no need to keep it around in
    Trash

--- locations.js
+++ locations.js
@@ -1096,13 +1096,7 @@ function unWrapFileManagerApp() {
  * up-to-date as the trash fills and is emptied over time.
  */
 var Trash = class DashToDock_Trash {
-    constructor() {
-        this._cancellable = new Gio.Cancellable();
-    }
-
     destroy() {
-        this._cancellable.cancel();
-        this._cancellable = null;
         this._trashApp?.destroy();
     }
 
@@ -1111,7 +1105,7 @@ var Trash = class DashToDock_Trash {
             return;
 
         this._trashApp = makeLocationApp({
-            appInfo: new TrashAppInfo(this._cancellable),
+            appInfo: new TrashAppInfo(new Gio.Cancellable()),
             fallbackIconName: FALLBACK_TRASH_ICON,
         });
     }
