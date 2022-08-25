5742974e26f6d90efb485d7115562a6f6f24751c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2022-08-25 18:00:56 +0200

    appSpread: Use utilities to manage methods monkey-patching

--- appSpread.js
+++ appSpread.js
@@ -5,108 +5,119 @@ const SearchController = imports.ui.searchController;
 const Workspace = imports.ui.workspace;
 const WorkspaceThumbnail = imports.ui.workspaceThumbnail;
 
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Utils = Me.imports.utils;
+
 var AppSpread = class AppSpread {
     constructor() {
         this.isInAppSpread = false;
         this.supported = true;
+        this.windows = [];
 
-        // gnome shell functions to temporarily replace while in app spread
-        this.originalWorkspaceIsOverviewWindow = Workspace?.Workspace?.prototype._isOverviewWindow;
-        this.originalThumbnailIsOverviewWindow = WorkspaceThumbnail?.WorkspaceThumbnail?.prototype._isOverviewWindow;
-        this.originalShouldTriggerSearch = SearchController?.SearchController?.prototype._shouldTriggerSearch;
         // fail early and do nothing, if mandatory gnome shell functions are missing
-        if (!this.originalWorkspaceIsOverviewWindow || !this.originalThumbnailIsOverviewWindow) {
+        if (!Workspace?.Workspace?.prototype._isOverviewWindow ||
+            !WorkspaceThumbnail?.WorkspaceThumbnail?.prototype._isOverviewWindow) {
             log('Dash to dock: Unable to temporarily replace shell functions for app spread - using previews instead');
             this.supported = false;
             return;
         }
 
-        // we need to hook into overview 'hidden' like this, in case appspread overview is hidden by choosing another app
-        // it should then do its cleanup too
-        this._hideOverviewHandlerId = Main.overview.connect('hidden', this._hideAppSpread.bind(this));
+        this._signalHandlers = new Utils.GlobalSignalsHandler();
+        this._methodInjections = new Utils.InjectionsHandler();
     }
 
-    disconnect() {
-        if (this._hideOverviewHandlerId) {
-            Main.overview.disconnect(this._hideOverviewHandlerId);
-        }
+    destroy() {
+        this._hideAppSpread();
+        this._signalHandlers.destroy();
+        this._methodInjections.destroy();
     }
 
-    toggle(appSpreadMetaWindows) {
+    toggle(windows) {
         if (Main.overview._shown) {
             Main.overview.hide(); // also triggers hook 'hidden'
         } else {
-            this._showAppSpread(appSpreadMetaWindows);
+            this._showAppSpread(windows);
         }
     }
 
-    _showAppSpread(appSpreadMetaWindows) {
+    _showAppSpread(windows) {
+        if (this.isInAppSpread)
+            return;
+
         // Checked in overview "hide" event handler _hideAppSpread
         this.isInAppSpread = true;
-        this.appSpreadMetaWindows = appSpreadMetaWindows;
+        this.windows = windows;
+        const appSpread = this;
 
-        // Filter workspaces to only show current app windows
-        const originalWorkspaceIsOverviewWindow = this.originalWorkspaceIsOverviewWindow;
-        Workspace.Workspace.prototype._isOverviewWindow = function(metaWindow) {
-            const originalResult = originalWorkspaceIsOverviewWindow(metaWindow);
-            return originalResult && appSpreadMetaWindows.indexOf(metaWindow) > -1;
-        };
+        // we need to hook into overview 'hidden' like this, in case app spread
+        // overview is hidden by choosing another app it should then do its
+        // cleanup too
+        this._signalHandlers.add(Main.overview, 'hidden', () => this._hideAppSpread());
 
-        // Filter thumbnails to only show current app windows
-        const originalThumbnailIsOverviewWindow = this.originalThumbnailIsOverviewWindow;
-        WorkspaceThumbnail.WorkspaceThumbnail.prototype._isOverviewWindow = function(metaWindowActor) {
-            const originalResult = originalThumbnailIsOverviewWindow(metaWindowActor);
-            return originalResult && appSpreadMetaWindows.indexOf(metaWindowActor.meta_window) > -1;
-        };
-
-        // If closing windows in AppSpread, and only one window left: exit app spread and focus remaining window (handled in _hideAppSpread)
-        this.destroyWindowId = global.window_manager.connect('destroy', (_, windowActor) => {
-            const metaWindow = windowActor.get_meta_window();
-            const index = appSpreadMetaWindows.indexOf(metaWindow);
-            if (index > -1) {
-                appSpreadMetaWindows.splice(index, 1);
+        this._methodInjections.add([
+            // Filter workspaces to only show current app windows
+            Workspace.Workspace.prototype, '_isOverviewWindow',
+            function (originalMethod, window) {
+                const isOverviewWindow = originalMethod.call(this, window);
+                return isOverviewWindow && appSpread.windows.includes(window);
             }
-            if (appSpreadMetaWindows.length === 1) {
+        ],
+        [
+            // Filter thumbnails to only show current app windows
+            WorkspaceThumbnail.WorkspaceThumbnail.prototype, '_isOverviewWindow',
+            function (originalMethod, windowActor) {
+                const isOverviewWindow = originalMethod.call(this, windowActor);
+                return isOverviewWindow && appSpread.windows.includes(windowActor.metaWindow);
+            }
+        ]);
+
+        // If closing windows in AppSpread, and only one window left:
+        // exit app spread and focus remaining window (handled in _hideAppSpread)
+        this._signalHandlers.add(global.window_manager, 'destroy', (_, windowActor) => {
+            const index = appSpread.windows.indexOf(windowActor.metaWindow);
+            if (index > -1)
+                appSpread.windows.splice(index, 1);
+
+            if (appSpread.windows.length <= 1)
                 Main.overview.hide();
-            }
         });
 
+        this._disableSearch();
+
         Main.overview.show();
-        // this._disableSearch();
     }
 
     _hideAppSpread() {
         if (!this.isInAppSpread)
             return;
 
-        this.isInAppSpread = false;
         // Restore original behaviour
-        // this._enableSearch();
-        Workspace.Workspace.prototype._isOverviewWindow = this.originalWorkspaceIsOverviewWindow;
-        WorkspaceThumbnail.WorkspaceThumbnail.prototype._isOverviewWindow = this.originalThumbnailIsOverviewWindow;
-        global.window_manager.disconnect(this.destroyWindowId);
-        // Check reason for leaving AppSpread was closing app windows and only one window left..
-        if (this.appSpreadMetaWindows.length === 1)
-            Main.activateWindow(this.appSpreadMetaWindows[0]);
+        this.isInAppSpread = false;
+        this._enableSearch();
+        this._methodInjections.clear();
+        this._signalHandlers.clear();
+
+        // Check reason for leaving AppSpread was closing app windows and only one window left...
+        if (this.windows.length === 1)
+            Main.activateWindow(this.windows[0]);
+
+        this.windows = [];
     }
 
     _disableSearch() {
+        if (!SearchController.SearchController.prototype._shouldTriggerSearch)
+            return;
+
         if (Main.overview.searchEntry)
             Main.overview.searchEntry.opacity = 0;
 
-        if (this.originalShouldTriggerSearch) {
-            SearchController.SearchController.prototype._shouldTriggerSearch = function() {
-                return false;
-            };
-        }
+        this._methodInjections.add(
+            SearchController.SearchController.prototype,
+            '_shouldTriggerSearch', () => false);
     }
 
     _enableSearch() {
         if (Main.overview.searchEntry)
             Main.overview.searchEntry.opacity = 255;
-
-        if (this.originalShouldTriggerSearch) {
-            SearchController.SearchController.prototype._shouldTriggerSearch = this.originalShouldTriggerSearch;
-        }
     }
 };
