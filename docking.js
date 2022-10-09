fbb9698d3da4c30562c34a6d30edfae679612b33
Marco Trevisan (Treviño) <mail@3v1n0.net>
2022-10-09 20:14:04 +0200

    docking: Do not wait for dash animation two times with no overview
    When not showing the overview we're waiting the double of time to show
    the dash, as the _runStartupAnimation() already uses a delay so there's
    no need to wait for previous animation to be completed to start a new
    one.

--- docking.js
+++ docking.js
@@ -2311,11 +2311,10 @@ var DockManager = class DashToDock_DockManager {
                         opacity: 255,
                         duration: STARTUP_ANIMATION_TIME,
                         mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-                        onComplete: () => {
-                            callback();
-                            this._runStartupAnimation();
-                        },
+                        onComplete: callback,
                     });
+
+                    this._runStartupAnimation();
                 });
         }
     }
