a48d5fe6bafe0f2170e3e6cd539d6b62287fc9c2
Marco Trevisan (Treviño) <mail@3v1n0.net>
2022-10-10 00:36:21 +0200

    docking: Also reduce the app grid area when in auto-hide mode
    We now need to also adapt the app-grid area to the available space in
    order to correctly allocate the left/right buttons in autohide mode.

--- docking.js
+++ docking.js
@@ -2135,8 +2135,6 @@ var DockManager = class DashToDock_DockManager {
 
                 if (!wsThumbnails && this.mainDock.position === St.Side.BOTTOM)
                     box.y2 -= spacing;
-            } else if (state === OverviewControls.ControlsState.APP_GRID) {
-                return box;
             }
 
             if (this.mainDock.isHorizontal || this.settings.dockFixed)
