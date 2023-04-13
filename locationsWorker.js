6cc9be96517afa2b485d617dc42d175c9f36a3ff
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-04-13 05:19:38 +0200

    locations: Use an external helper to get the location handler
    Getting the default location handler has to be a sync operation because
    the shell uses a sync operations to launch applications.
    However, this may have lead to hanging the shell, making it unresponsive.
    
    We handled this within the shell itself, but to get it working we had to
    keep keep the main loop busy, leading to other issues (see LP: #2013070)
    
    As per this, use an external helper that queries the files paths so that
    we don't have to make the shell main loop to iterate, but all this is done
    in an external process that we call still synchronously but with a minimal
    timeout so that the main session won't ever hang.
    
    Improves fix for: #1949
    LP: #2013070

---
+++ locationsWorker.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env gjs
+
+const { GLib, Gio } = imports.gi;
+
+const currentPath = GLib.path_get_dirname(new Error().fileName);
+imports.searchPath.unshift(currentPath);
+
+const GJS_SUPPORTS_FILE_IFACE_PROMISES = imports.system.version >= 17101;
+
+if (GJS_SUPPORTS_FILE_IFACE_PROMISES)
+    Gio._promisify(Gio.File.prototype, 'query_default_handler_async');
+
+function getHandlerAppAsync(location, cancellable) {
+    if (!location)
+        return null;
+
+    if (!GJS_SUPPORTS_FILE_IFACE_PROMISES) {
+        Gio._promisify(location.constructor.prototype,
+            'query_default_handler_async',
+            'query_default_handler_finish');
+    }
+
+    return location.query_default_handler_async(
+        GLib.PRIORITY_DEFAULT, cancellable);
+}
+
+function main(argv) {
+    if (argv.length < 1) {
+        const currentBinary = GLib.path_get_basename(new Error().fileName);
+        printerr(`Usage: ${currentBinary} <action> uri [ --timeout <value> ]`);
+        return 1;
+    }
+
+    const [action, uri] = argv;
+    let timeout = 200;
+
+    if (action !== 'handler')
+        throw new TypeError(`Unexpected action ${action}`);
+
+    for (let i = 1; i < argv.length; ++i) {
+        if (argv[i] === '--timeout' && i < argv.length - 1)
+            timeout = argv[++i];
+    }
+
+    const location = Gio.File.new_for_uri(uri);
+    const cancellable = new Gio.Cancellable();
+
+    // GVfs providers could hang when querying the file information, so we
+    // workaround this by using the async API in a sync way, but we need to
+    // use a timeout to avoid this to hang forever, better than hang the
+    // shell.
+    let handler, error, launchMaxWaitId;
+    Promise.race([
+        getHandlerAppAsync(location, cancellable),
+        new Promise((resolve, reject) => {
+            launchMaxWaitId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, timeout, () => {
+                launchMaxWaitId = 0;
+                cancellable.cancel();
+                reject(new GLib.Error(Gio.IOErrorEnum,
+                    Gio.IOErrorEnum.TIMED_OUT,
+                    `Searching for ${location.get_uri()} handler took too long`));
+                return GLib.SOURCE_REMOVE;
+            });
+        }),
+    ]).then(h => (handler = h)).catch(e => (error = e));
+
+    while (handler === undefined && error === undefined)
+        GLib.MainContext.default().iteration(false);
+
+    if (launchMaxWaitId)
+        GLib.source_remove(launchMaxWaitId);
+
+    if (error) {
+        printerr(error.message);
+        logError(error);
+        return error.code ? error.code : GLib.MAXUINT8;
+    }
+
+    print(handler.get_id());
+
+    return 0;
+}
+
+main(ARGV);
