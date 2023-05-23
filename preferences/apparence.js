2e9a4c408a2b294c8f4cc64c2ec274835675f848
Ned-Tom <tdj800@gmail.com>
2023-05-23 19:22:04 +0200

    Made some options in Apparence work

--- preferences/apparence.js
+++ preferences/apparence.js
@@ -8,8 +8,37 @@ const Me = ExtensionUtils.getCurrentExtension();
 var Apparence = GObject.registerClass({
     GTypeName: 'Apparence',
     Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'apparence.ui'])}`,
+    InternalChildren: [
+        'customThemeShrink',
+        'disableOverviewOnStartup',
+        'applyCustomTheme',
+        'customBackgroundColor',
+        'backgroundColor'
+    ]
 }, class Apparence extends Adw.PreferencesPage {
-    constructor() {
+    constructor(settings) {
         super({});
+
+        settings.bind(
+            'custom-theme-shrink', this._customThemeShrink, 'active',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+        settings.bind(
+            'disable-overview-on-startup', this._disableOverviewOnStartup, 'active',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+        settings.bind(
+            'apply-custom-theme', this._applyCustomTheme, 'active',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+        settings.bind(
+            'custom-background-color', this._customBackgroundColor, 'enable-expansion',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+        settings.bind(
+            'background-color', this._backgroundColor, 'rgba',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+
     }
 });
