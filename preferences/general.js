3bd97909cf54d674aba149aef9e565bc273e985a
Ned-Tom <tdj800@gmail.com>
2023-06-04 21:27:03 +0200

    Added some gui bindings in the general page

--- preferences/general.js
+++ preferences/general.js
@@ -8,9 +8,23 @@ const Me = ExtensionUtils.getCurrentExtension();
 var General = GObject.registerClass({
     GTypeName: 'General',
     Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'general.ui'])}`,
-    InternalChildren: []
+    InternalChildren: [
+        // 'position', //dock-position
+        'showAllScreens', //multi-monitor (b)
+        'disableOverviewOnStartup' //disable-overview-on-startup (b)
+    ]
 }, class General extends Adw.PreferencesPage {
     constructor(settings) {
         super({});
+
+        settings.bind(
+            'multi-monitor', this._showAllScreens, 'active',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+
+        settings.bind(
+            'disable-overview-on-startup', this._disableOverviewOnStartup, 'active',
+            Gio.SettingsBindFlags.DEFAULT
+        );
     }
 });
