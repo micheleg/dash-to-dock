50d4800150c5bd3bd4097e095004aafbd948ef27
Ned-Tom <tdj800@gmail.com>
2023-06-11 20:36:05 +0200

    Added Name and description to about page

--- preferences/about.js
+++ preferences/about.js
@@ -10,13 +10,21 @@ var About = GObject.registerClass(
         GTypeName: 'About',
         Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'about.ui'])}`,
         InternalChildren: [
+            'name',
             'version',
-            'author'
+            'author',
+            'description'
         ]
     }, 
     class About extends Adw.PreferencesPage {
         constructor(settings) {
             super({});
+
+            this._name.label = Me.metadata.name.toString();
+            this._version.label = Me.metadata.version.toString();
+            this._author.label = Me.metadata['original-author'].toString();
+            this._description.set_subtitle(Me.metadata.description.toString());
+            
         };
     }
 );
