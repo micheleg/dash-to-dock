6beae12ddbeef40bed7bbc4798240fb63dada95a
Ned-Tom <tdj800@gmail.com>
2023-08-31 16:47:12 +0200

    Add icon theme and improve imports

--- prefs.js
+++ prefs.js
@@ -1,30 +1,41 @@
+// prefs.js
+// Import dependencies
 import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import Gdk from 'gi://Gdk';
+import Gtk from 'gi://Gtk';
+
+// Import pages
 import { General } from './preferences/general.js';
 import { Launchers } from './preferences/launchers.js';
 import { Behavior } from './preferences/behavior.js';
 import { Appearance } from './preferences/appearance.js';
 
+// Generate window
 export default class DockPreferences extends ExtensionPreferences {
     constructor(metadata) {
+        // init whit metadata
         super(metadata);
-
+        // init tramslations
         this.initTranslations('dashtodock');
-
-        // // load the icon theme
-        // let iconPath = imports.misc.extensionUtils.getCurrentExtension()
-        //     .dir.get_child("icons").get_path();
-        // let iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
-        // iconTheme.add_search_path(iconPath);
     }
 
     fillPreferencesWindow(window) {
-        window._settings = this.getSettings('org.gnome.shell.extensions.dash-to-dock');
+        // Helpers
+        const settings = this.getSettings('org.gnome.shell.extensions.dash-to-dock');
+        const iconPath = `${this.path}/icons`;
 
-        window.add(new General(window._settings));
-        window.add(new Launchers(window._settings));
-        window.add(new Behavior(window._settings));
-        window.add(new Appearance(window._settings));
+        // Add new icons
+        const iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
+        if (!iconTheme.get_search_path().includes(iconPath))
+            iconTheme.add_search_path(iconPath);
+
+        // Add pref pages
+        window.add(new General(settings));
+        window.add(new Launchers(settings));
+        window.add(new Behavior(settings));
+        window.add(new Appearance(settings));
         
-        window.search_enabled = true;
+        // window.search_enabled = true;
+        window.set_search_enabled(true);
     }
 };
