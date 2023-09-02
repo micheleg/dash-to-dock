067c0d23c4cc7bab55ce11991e57afaa1bd82b84
Ned-Tom <tdj800@gmail.com>
2023-09-02 15:21:48 +0200

    Add About page and cleanup

---
+++ preferences/about.js
@@ -0,0 +1,124 @@
+'use strict';
+
+import { gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
+import * as Config from 'resource:///org/gnome/Shell/Extensions/js/misc/config.js'
+
+import Gio from 'gi://Gio'
+import GLib from 'gi://GLib';
+import Adw from 'gi://Adw'
+import GObject from 'gi://GObject'
+import Gtk from 'gi://Gtk'
+
+import { d2dprefsspage } from '../conveniences/d2dprefsspage.js'
+
+const About = GObject.registerClass({
+    GTypeName: 'About'
+},
+class About extends d2dprefsspage{
+
+    constructor(settings,metadata){
+        super(settings)
+
+        this._settings = settings
+
+        // Set headerbar page info
+        this.title = _('About')
+        this.icon_name = 'help-about-symbolic'
+        
+
+        // Project Logo, title, description
+        const projectHeaderGroup = new Adw.PreferencesGroup()
+        const projectHeaderBox = new Gtk.Box({
+            orientation: Gtk.Orientation.VERTICAL,
+            hexpand: false,
+            vexpand: false,
+        })
+
+        const projectImage = new Gtk.Image({
+            margin_bottom: 5,
+            icon_name: 'dash-to-dock',
+            pixel_size: 100,
+        })
+
+        const projectTitleLabel = new Gtk.Label({
+            label: _('Dash to Dock'),
+            css_classes: ['title-1'],
+            vexpand: true,
+            valign: Gtk.Align.FILL,
+        })
+
+        const projectDescriptionLabel = new Gtk.Label({
+            label: 'A dock for the Gnome Shell.',
+            hexpand: false,
+            vexpand: false,
+        })
+        projectHeaderBox.append(projectImage)
+        projectHeaderBox.append(projectTitleLabel)
+        projectHeaderBox.append(projectDescriptionLabel)
+        projectHeaderGroup.add(projectHeaderBox)
+
+        this.add(projectHeaderGroup)
+        
+        // details
+
+        const details = new Adw.PreferencesGroup()
+        this.add(details)
+
+        const versionRow = new Adw.ActionRow({
+            title: _('Dash to Dock Version')
+        })
+        details.add(versionRow)
+        versionRow.add_suffix(new Gtk.Label({
+            label: metadata.version.toString(),
+            css_classes: ['dim-label']
+        }))
+        
+        const gnomeVersionRow = new Adw.ActionRow({
+            title: _('Gnome Version')
+        })
+        details.add(gnomeVersionRow)
+        gnomeVersionRow.add_suffix(new Gtk.Label({
+            label: Config.PACKAGE_VERSION.toString(),
+            css_classes: ['dim-label']
+        }));
+
+        const osRow = new Adw.ActionRow({
+            title: _('OS Name')
+        })
+        details.add(osRow)
+        const name = GLib.get_os_info('NAME')
+        const prettyName = GLib.get_os_info('PRETTY_NAME')
+
+        osRow.add_suffix(new Gtk.Label({
+            label: prettyName ? prettyName : name,
+            css_classes: ['dim-label']
+        }))
+
+        const windowVersionRow = new Adw.ActionRow({
+            title: _('Windowsing System')
+        })
+        details.add(windowVersionRow)
+        windowVersionRow.add_suffix(new Gtk.Label({
+            label: GLib.getenv('XDG_SESSION_TYPE') === 'wayland' ? 'Wayland' : 'X11',
+            css_classes: ['dim-label']
+        }))
+
+        // const sourceLinkRow = new Adw.ActionRow({
+        //     title: _('Source code'),
+        //     activatable: true
+        // })
+        // details.add(sourceLinkRow)
+        // sourceLinkRow.connect('activated', () => {
+        //     Gtk.show_uri(window, metadata.url, Gdk.CURRENT_TIME)
+        // })
+        // const linkImage = new Gtk.Image({
+        //     icon_name: 'adw-external-link-symbolic',
+        //     valign: Gtk.Align.CENTER
+        // })
+        // sourceLinkRow.add_suffix(linkImage)
+        
+        return this
+    }
+})
+
+export { About }
--- prefs.js
+++ prefs.js
@@ -1,41 +1,44 @@
 // prefs.js
 // Import dependencies
-import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
-import Gdk from 'gi://Gdk';
-import Gtk from 'gi://Gtk';
+import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
+import Gdk from 'gi://Gdk'
+import Gtk from 'gi://Gtk'
 
 // Import pages
-import { General } from './preferences/general.js';
-import { Launchers } from './preferences/launchers.js';
-import { Behavior } from './preferences/behavior.js';
-import { Appearance } from './preferences/appearance.js';
+import { General } from './preferences/general.js'
+import { Launchers } from './preferences/launchers.js'
+import { Behavior } from './preferences/behavior.js'
+import { Appearance } from './preferences/appearance.js'
+import { About } from './preferences/about.js'
 
 // Generate window
 export default class DockPreferences extends ExtensionPreferences {
     constructor(metadata) {
         // init whit metadata
-        super(metadata);
+        super(metadata)
+        this.metadata = metadata
         // init tramslations
-        this.initTranslations('dashtodock');
+        this.initTranslations('dashtodock')
     }
 
     fillPreferencesWindow(window) {
         // Helpers
-        const settings = this.getSettings('org.gnome.shell.extensions.dash-to-dock');
-        const iconPath = `${this.path}/icons`;
+        const settings = this.getSettings('org.gnome.shell.extensions.dash-to-dock')
+        const iconPath = `${this.path}/icons`
 
         // Add new icons
-        const iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
+        const iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default())
         if (!iconTheme.get_search_path().includes(iconPath))
-            iconTheme.add_search_path(iconPath);
+            iconTheme.add_search_path(iconPath)
 
         // Add pref pages
-        window.add(new General(settings));
-        window.add(new Launchers(settings));
-        window.add(new Behavior(settings));
-        window.add(new Appearance(settings));
+        window.add(new General(settings))
+        window.add(new Launchers(settings))
+        window.add(new Behavior(settings))
+        window.add(new Appearance(settings))
+        window.add(new About(settings,this.metadata))
         
         // window.search_enabled = true;
-        window.set_search_enabled(true);
+        window.set_search_enabled(true)
     }
-};
+}
