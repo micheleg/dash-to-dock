88abaf9215becfadedd349ae80b1a547bcb68c7a
Ned-Tom <tdj800@gmail.com>
2023-09-03 12:38:24 +0200

    Add new input button and spinbutton row

--- conveniences/d2dprefsspage.js
+++ conveniences/d2dprefsspage.js
@@ -6,37 +6,37 @@ import Gdk from 'gi://Gdk'
 
 const d2dprefsspage = GObject.registerClass({
     GTypeName: 'd2dprefsspage'
-},class d2dprefsspage extends Adw.PreferencesPage{
+}, class d2dprefsspage extends Adw.PreferencesPage {
 
-    _toggleRow(setting,title,subtitle = ''){
+    _toggleRow(setting, title, subtitle = '') {
         let row = new Adw.SwitchRow({
             title: title,
             subtitle: subtitle,
-        });
-        
+        })
+
         this._settings.bind(
             setting, row, 'active',
             Gio.SettingsBindFlags.DEFAULT
-        );
+        )
 
         return row
     }
 
-    _toggleInvRow(setting,title,subtitle = ''){
+    _toggleInvRow(setting, title, subtitle = '') {
         let row = new Adw.SwitchRow({
             title: title,
             subtitle: subtitle,
-        });
-        
+        })
+
         this._settings.bind(
             setting, row, 'active',
             Gio.SettingsBindFlags.INVERT_BOOLEAN
-        );
+        )
 
         return row
     }
 
-    _listRow(setting,items,title,subtitle = ''){
+    _listRow(setting, items, title, subtitle = '') {
         const myListrowdata = new Gtk.StringList()
 
         items.forEach((value) => {
@@ -56,7 +56,7 @@ const d2dprefsspage = GObject.registerClass({
         return myListrow
     }
 
-    _expandRow(setting,title,subtitle = ''){
+    _expandRow(setting, title, subtitle = '') {
         const row = new Adw.ExpanderRow({
             title: title,
             subtitle: subtitle,
@@ -70,7 +70,7 @@ const d2dprefsspage = GObject.registerClass({
         return row
     }
 
-    _colorRow(setting,title,subtitle = ''){
+    _colorRow(setting, title, subtitle = '') {
         const row = new Adw.ActionRow({
             title: title,
             subtitle: subtitle,
@@ -91,8 +91,60 @@ const d2dprefsspage = GObject.registerClass({
         row.add_suffix(color);
         return row
     }
-    
-    constructor(settings){
+
+    _spinBTNRow(setting,adjustment, title, subtitle = '') {
+        const row = new Adw.ActionRow({
+            title: title,
+            subtitle: subtitle
+        })
+
+        const rowScale = new Gtk.SpinButton({
+            adjustment: adjustment,
+            // adjustment: new Gtk.Adjustment({
+            //     lower: 0,
+            //     upper: 10000,
+            //     step_increment: 250,
+            //     page_increment: 1,
+            //     page_size: 0,
+            // }),
+            digits: 0,
+            valign: Gtk.Align.CENTER
+        })
+        rowScale.set_value(this._settings.get_double(setting))
+        rowScale.connect('value-changed', () => {
+            this._settings.set_int(setting, rowScale.get_double())
+        })
+        // paddingScale.set_value(this._settings.get_int('button-padding'));
+        // paddingScale.connect('value-changed', () => {
+        //     this._settings.set_int('button-padding', paddingScale.get_value());
+        // });
+        row.add_suffix(rowScale)
+
+        return row
+    }
+
+    _inputRow(setting, title, subtitle = '') {
+        const row = new Adw.ActionRow({
+            title: title,
+            subtitle: subtitle
+        })
+        // const textBox = new Gtk.Text({
+        const textBox = new Gtk.Entry({
+            placeholderText: 'some text',
+            valign: Gtk.Align.CENTER
+        })
+
+        this._settings.bind(
+            setting, row, 'text',
+            Gio.SettingsBindFlags.DEFAULT
+        )
+
+        row.add_suffix(textBox);
+
+        return row
+    }
+
+    constructor(settings) {
         super()
 
         this._settings = settings
