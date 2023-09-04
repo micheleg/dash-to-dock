f4ff851bd257c35e4e9958a46be124beb39a42f5
Ned-Tom <tdj800@gmail.com>
2023-09-04 12:10:38 +0200

    Add dock scale an size options to general page

--- preferences/general.js
+++ preferences/general.js
@@ -15,52 +15,12 @@ const General = GObject.registerClass({
     GTypeName: 'General'
 },
 class General extends d2dprefsspage{
-
-    _monitorsConfig = new MonitorsConfig()
-
-    _updateMonitorsSettings() {
-        // Monitor options
-        const preferredMonitor = this._settings.get_int('preferred-monitor')
-        const preferredMonitorByConnector = this._settings.get_string('preferred-monitor-by-connector')
-        const dockMonitorCombo = this._builder.get_object('dock_monitor_combo')
-
-        this._monitors = []
-        dockMonitorCombo.remove_all()
-        let primaryIndex = -1
-
-        // Add connected monitors
-        for (const monitor of this._monitorsConfig.monitors) {
-            if (!monitor.active && monitor.index !== preferredMonitor)
-                continue;
-
-            if (monitor.isPrimary) {
-                dockMonitorCombo.append_text(
-                    /* Translators: This will be followed by Display Name - Connector. */
-                    `${__('Primary monitor: ') + monitor.displayName} - ${
-                        monitor.connector}`)
-                primaryIndex = this._monitors.length
-            } else {
-                dockMonitorCombo.append_text(
-                    /* Translators: Followed by monitor index, Display Name - Connector. */
-                    `${__('Secondary monitor ') + (monitor.index + 1)} - ${
-                        monitor.displayName} - ${monitor.connector}`)
-            }
-
-            this._monitors.push(monitor)
-
-            if (monitor.index === preferredMonitor ||
-                (preferredMonitor === -2 && preferredMonitorByConnector === monitor.connector))
-                dockMonitorCombo.set_active(this._monitors.length - 1)
-        }
-
-        if (dockMonitorCombo.get_active() < 0 && primaryIndex >= 0)
-            dockMonitorCombo.set_active(primaryIndex)
-    }
-
     constructor(settings,extPrefs){
         super(settings)
 
         this._settings = settings
+
+        this._monitorsConfig = new MonitorsConfig()
 
         // Set headerbar page info
         this.title = _('General')
@@ -82,18 +42,30 @@ class General extends d2dprefsspage{
         });
         PosGroup.add(monitorSelector)
 
-        // const monitorSelectorBox = new Gtk.ComboBoxText();
-        // monitorSelectorBox.valign = 'center';
-        // monitorSelector.append(monitorSelectorBox);
+        const monitorSelectorList = new Gtk.ComboBoxText({
+            valign: Gtk.Align.CENTER
+        })
+        monitorSelectorList.selected = this._settings.get_enum('dock-position')
 
-        // monitorSelectorBox.selected = this._settings.get_enum('dock-position');
+
+        // monitorSelectorList.append('1',_('some translatebale item 1'))
+
+        for (const monitor of this._monitorsConfig.monitors) {
+            monitorSelectorList.append(monitor.index, monitor.displayName)
+        }
+        // this._monitors.forEach((value) => {
+        //     monitorSelectorList.append(value.index, value.displayName)
+        // })
+
+
+        monitorSelector.add_suffix(monitorSelectorList)
 
         this._monitorsConfig.connect('updated',
             () => this._updateMonitorsSettings())
-        // this._settings.connect('changed::preferred-monitor',
-        //     () => this._updateMonitorsSettings());
-        // this._settings.connect('changed::preferred-monitor-by-connector',
-        //     () => this._updateMonitorsSettings());
+        this._settings.connect('changed::preferred-monitor',
+            () => this._updateMonitorsSettings());
+        this._settings.connect('changed::preferred-monitor-by-connector',
+            () => this._updateMonitorsSettings());
 
 
         // Multi monitor toggle
@@ -121,22 +93,14 @@ class General extends d2dprefsspage{
         })
         this.add(AHGroup)
     
-
         // Inteligent autohide
-
-
-        // const row = new Adw.SwitchRow({
-        //     title: _('Inteligent autohide'),
-        //     subtitle: _('Hide the dock when it obstrucs a windows of the current appliation.'),
-        // });
-        // AHGroup.add(row);
-
-
         const autoHideRow = new Adw.ActionRow({
             title: _('Inteligent autohide'),
             subtitle: _('Hide the dock when it obstrucs a windows of the current appliation.'),
+            activatable: true
         })
         AHGroup.add(autoHideRow)
+
 
         const goNextImage = new Gtk.Image({
             gicon: Gio.icon_new_for_string('go-next-symbolic'),
@@ -147,42 +111,43 @@ class General extends d2dprefsspage{
         })
         autoHideRow.add_suffix(goNextImage)
 
-
-
-        // const autoHideRow = new SettingRow({
-        //     title: _('Menu Layout'),
-        //     subtitle: _('Choose a layout style for the menu'),
-        //     icon_name: 'settings-layouts-symbolic',
-        // });     
-
-        // const autoHidePage = new autoHidePageClass(this._settings);
-        // autoHideRow.settingPage = autoHidePage;
-
-
-        // autoHideRow.connect('activated', () => {
-        //     if (settingPage.setActiveLayout)
-        //         settingPage.setActiveLayout(this._settings.get_enum('menu-layout'));
-
-        //     this._window.present_subpage(settingPage);
-        //     settingPage.resetScrollAdjustment();
-        // });
-
-        // autoHideRow.settingPage.connect('response', (_w, response) => {
-        //     // if (response === Gtk.ResponseType.APPLY) {
-        //     //     const layoutName = SettingsUtils.getMenuLayoutName(this._settings.get_enum('menu-layout'));
-        //     //     this.tweaksRow.title = _('%s Layout Tweaks').format(_(layoutName));
-        //     // }
-        // });
-
-
-
         // ## Dock scale and size group
         const sizeGroup = new Adw.PreferencesGroup({
             title: _('Dock scale and size')
         })
         this.add(sizeGroup)
 
-        
+
+        // dock size limit
+        // Panel mode: extent to the screen edge
+        const pannelMode = this._expandRow(
+            'extend-height',
+            _('Panel mode'),
+            _('Extent dock to the screen edge')
+        )
+        // Put show Aplications in a doc edge when using pannel mode
+        pannelMode.add_row(this._toggleRow(
+            'show-apps-always-in-the-edge',
+            _('Move edge of the dock'),
+            _('Show application button on the edge when using centered panel mode')
+        ))
+        // Place icons tot the center
+        pannelMode.add_row(this._toggleRow(
+            'always-center-icons',
+            _('Place icons tot the center')
+        ))
+        sizeGroup.add(pannelMode)
+
+        // icon side limt
+        // fix icon size: scroll to reveal other icons
+        sizeGroup.add(this._toggleRow(
+            'icon-size-fixed',
+            _('fix icon size'),
+            _('scroll to reveal other icons')
+        ));
+
+        // preview scale
+       
         return this
     }
 })
