31804384f6159fc969919e3ee71fef7d5301710e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-04-19 01:46:00 +0200

    locations: Do not cancel operations after a volume has been umounted / removed
    In case we're unmounting a volume we were optionally removing its icon
    but this was also leading cancelling the operation that in the Gio queue
    caused the action not to be fully performed.
    
    So once, we've removed a volume or umounted a mount, we don't really
    care about cancelling the ongoing operations since they should be done
    already.
    
    LP: #1993054

--- locations.js
+++ locations.js
@@ -93,7 +93,7 @@ var LocationAppInfo = GObject.registerClass({
             Gio.Icon.$gtype),
         'cancellable': GObject.ParamSpec.object(
             'cancellable', 'cancellable', 'cancellable',
-            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            GObject.ParamFlags.READWRITE,
             Gio.Cancellable.$gtype),
     },
 }, class LocationAppInfo extends Gio.DesktopAppInfo {
@@ -1328,6 +1328,8 @@ var Removables = class DashToDock_Removables {
             appInfo.volume === volume);
         if (volumeIndex !== -1) {
             const [volumeApp] = this._volumeApps.splice(volumeIndex, 1);
+            // We don't care about cancelling the ongoing operations from now on.
+            volumeApp.appInfo.cancellable = null;
             volumeApp.destroy();
             this.emit('changed');
         }
