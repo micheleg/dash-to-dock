bf2137f05ccd1204e13c5beaf66f535e2d238d97
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-08-12 19:12:41 -0400

    notificationsMonitor: Try to get appId from the source if set

--- notificationsMonitor.js
+++ notificationsMonitor.js
@@ -92,8 +92,9 @@ export class NotificationsMonitor {
 
                 source.notifications.forEach(notification => {
                     const app = notification.source?.app ?? notification.source?._app;
+                    const appId = app?.id ?? app?._appId;
 
-                    if (app?.id) {
+                    if (appId) {
                         if (notification.resident) {
                             if (notification.acknowledged)
                                 return;
@@ -106,8 +107,8 @@ export class NotificationsMonitor {
                         this._signalsHandler.addWithLabel(Labels.NOTIFICATIONS,
                             notification, 'destroy', () => this._checkNotifications());
 
-                        this._appNotifications[app.id] =
-                            (this._appNotifications[app.id] ?? 0) + 1;
+                        this._appNotifications[appId] =
+                            (this._appNotifications[appId] ?? 0) + 1;
                     }
                 });
             });
