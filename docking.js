micheleg/dash-to-dock

commit 13ca201826453bc705e7cbec98f14ad2f2f6e1b9

docking: Simplify the creation and destruction of the docks
We were not tracking the dock destruction events so we may have
potentially accessed to destroyed dock's when iterating over them

Helps with: lp:2148339

docking.js
@@ -1795,7 +1795,7 @@ export class DockManager {
     }
 
     get mainDock() {
-        return this._allDocks.length ? this._allDocks[0] : null;
+        return this._allDocks[0] ?? null;
     }
 
     get removables() {
@@ -2070,15 +2070,10 @@ export class DockManager {
 
 
         // First we create the main Dock, to get the extra features to bind to this one
-        let dock = new DockedDash({
+        this._createDock({
             monitorIndex: this._preferredMonitorIndex,
             isMain: true,
         });
-        this._allDocks.push(dock);
-
-        // connect app icon into the view selector
-        dock.dash.showAppsButton.connect('notify::checked',
-            this._onShowAppsButtonToggled.bind(this));
 
         // Make the necessary changes to Main.overview.dash
         this._prepareMainDash();
@@ -2091,11 +2086,8 @@ export class DockManager {
             for (let iMon = 0; iMon < nMon; iMon++) {
                 if (iMon === this._preferredMonitorIndex)
                     continue;
-                dock = new DockedDash({monitorIndex: iMon});
-                this._allDocks.push(dock);
-                // connect app icon into the view selector
-                dock.dash.showAppsButton.connect('notify::checked',
-                    this._onShowAppsButtonToggled.bind(this));
+
+                this._createDock({monitorIndex: iMon});
             }
         }
 
@@ -2105,6 +2097,24 @@ export class DockManager {
         this._keyboardShortcuts = new KeyboardShortcuts();
 
         this.emit('docks-ready');
+    }
+
+    _createDock(params) {
+        const dock = new DockedDash(params);
+        this._allDocks.push(dock);
+
+        // connect app icon into the view selector
+        dock.dash.showAppsButton.connectObject('notify::checked',
+            button => this._onShowAppsButtonToggled(button), dock);
+
+        const id = dock.connect('destroy', () => {
+            dock.disconnect(id);
+            const index = this._allDocks.indexOf(dock);
+            if (index !== -1)
+                this._allDocks.splice(index, 1);
+        });
+
+        return dock;
     }
 
     _prepareStartupAnimation() {
@@ -2460,17 +2470,13 @@ export class DockManager {
     }
 
     _deleteDocks() {
-        if (!this._allDocks.length)
-            return;
-
         // Remove extra features
-        this._workspaceIsolation.destroy();
-        this._keyboardShortcuts.destroy();
-        this._desktopIconsUsableArea.resetMargins();
+        this._workspaceIsolation?.destroy();
+        this._keyboardShortcuts?.destroy();
+        this._desktopIconsUsableArea?.resetMargins();
 
         // Delete all docks
         this._allDocks.forEach(d => d.destroy());
-        this._allDocks = [];
 
         this.emit('docks-destroyed');
     }

commit 5d077e9f1b754cbdedce67cca46da3f5f8e4f12b

docking: Iterate over a docks snapshot on destruction
Each dock destroy signal handler removes the dock from the allDocks
array, but doing this while looping on it may lead to skipping elements
and it's not safe.

So loop over a snapshot instead.

Fixes: 13ca2018

docking.js
@@ -2488,7 +2488,7 @@ export class DockManager {
         this._desktopIconsUsableArea?.resetMargins();
 
         // Delete all docks
-        this._allDocks.forEach(d => d.destroy());
+        [...this._allDocks].forEach(d => d.destroy());
 
         this.emit('docks-destroyed');
     }