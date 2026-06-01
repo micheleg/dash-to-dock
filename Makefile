micheleg/dash-to-dock

commit 2bc44a6addc1c58360b12038ef5a0bec14403548

Makefile: Remember gschemas.compiled for local installs
Or else the extension won't load at all.

Closes: https://github.com/micheleg/dash-to-dock/issues/2595
Fixes: 1ad2628e32 ("Makefile: Do not ship settings schema on zip file")

Makefile
@@ -107,6 +107,7 @@ ifeq ($(INSTALLTYPE),system)
 	cp -r ./schemas/*.gschema.xml $(SHARE_PREFIX)/glib-2.0/schemas
 	cp -r ./_build/locale/* $(SHARE_PREFIX)/locale
 endif
+	cp schemas/gschemas.compiled $(INSTALLBASE)/$(INSTALLNAME)/schemas/
 	-rm -fR _build
 	echo done
 

commit 330e7084a6402f12a127ba862740cb7a46e8f7e9

Don't install gschemas.compiled for $(INSTALLTYPE)==system
Because $(INSTALLTYPE)==system starts by deleting the target directory
so our copy would fail.

Closes: https://github.com/micheleg/dash-to-dock/issues/2602
Fixes: 2bc44a6add ("Makefile: Remember gschemas.compiled for local installs")

Makefile
@@ -106,8 +106,9 @@ ifeq ($(INSTALLTYPE),system)
 	mkdir -p $(SHARE_PREFIX)/glib-2.0/schemas $(SHARE_PREFIX)/locale
 	cp -r ./schemas/*.gschema.xml $(SHARE_PREFIX)/glib-2.0/schemas
 	cp -r ./_build/locale/* $(SHARE_PREFIX)/locale
-endif
+else
 	cp schemas/gschemas.compiled $(INSTALLBASE)/$(INSTALLNAME)/schemas/
+endif
 	-rm -fR _build
 	echo done
 