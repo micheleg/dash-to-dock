ba5ed27926fd79759dace19de2f1d8ed02af852a
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-07-25 00:28:22 +0200

    docking: Ru-use overview controls property

--- docking.js
+++ docking.js
@@ -2500,7 +2500,7 @@ export class DockManager {
                     this._runStartupAnimation();
                     if (this._settings.disableOverviewOnStartup) {
                         this._propertyInjections.removeWithLabel(Labels.STARTUP_ANIMATION);
-                        Main.overview._overview.controls._stateAdjustment.value =
+                        this.overviewControls._stateAdjustment.value =
                             OverviewControls.ControlsState.HIDDEN;
                     }
                 });
